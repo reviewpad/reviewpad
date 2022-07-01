65161bbf444ddde762b1add21e4d744d92db0f00
Cheila Alves <calves.200025@gmail.com>
2022-07-01 11:29:44 +0100

    Add unit tests for merge action builtin

---
+++ plugins/aladino/actions/merge_test.go
@@ -0,0 +1,92 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_actions_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var merge = plugins_aladino.PluginBuiltIns().Actions["merge"].Code
+
+type MergeRequestPostBody struct {
+	MergeMethod string `json:"merge_method"`
+}
+
+func TestMerge_WhenMergeMethodIsInvalid(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{aladino.BuildStringValue("INVALID")}
+	err = merge(mockedEnv, args)
+
+	assert.EqualError(t, err, "merge: unexpected argument INVALID")
+}
+
+func TestMerge_WhenNoMergeMethodIsProvided(t *testing.T) {
+	wantMergeMethod := "merge"
+	var gotMergeMethod string
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatchHandler(
+			mock.PutReposPullsMergeByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				rawBody, _ := ioutil.ReadAll(r.Body)
+				body := MergeRequestPostBody{}
+
+				json.Unmarshal(rawBody, &body)
+
+				gotMergeMethod = body.MergeMethod
+			}),
+		),
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{}
+	err = merge(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantMergeMethod, gotMergeMethod)
+}
+
+func TestMerge_WhenMergeMethodIsProvided(t *testing.T) {
+	wantMergeMethod := "rebase"
+	var gotMergeMethod string
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatchHandler(
+			mock.PutReposPullsMergeByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				rawBody, _ := ioutil.ReadAll(r.Body)
+				body := MergeRequestPostBody{}
+
+				json.Unmarshal(rawBody, &body)
+
+				gotMergeMethod = body.MergeMethod
+			}),
+		),
+	)
+
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{aladino.BuildStringValue(wantMergeMethod)}
+	err = merge(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantMergeMethod, gotMergeMethod)
+}
