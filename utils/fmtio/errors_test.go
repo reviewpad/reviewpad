5d9410eb3883cda2b140116e6d999ba1f4500836
Cheila Alves <calves.200025@gmail.com>
2022-07-04 16:20:58 +0100

    Add unit tests for utils fmtio

---
+++ utils/fmtio/errors_test.go
@@ -0,0 +1,20 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package fmtio_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/utils/fmtio"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrorf(t *testing.T) {
+	wantError := errors.New("[test] testing error message")
+	gotError := fmtio.Errorf("test", "testing %v", "error message")
+
+	assert.Equal(t, wantError, gotError)
+}
