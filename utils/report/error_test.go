04ba7de3118785df1245eb14386a8a21822e08a4
Cheila Alves <calves.200025@gmail.com>
2022-07-04 16:33:58 +0100

    Add unit tests for report error

---
+++ utils/report/error_test.go
@@ -0,0 +1,19 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package report_test
+
+import (
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/utils/report"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestError(t *testing.T) {
+	wantError := "Error occurred! Details:\nThere is something wrong with testing"
+	gotError := report.Error("There is something wrong with %v", "testing")
+
+	assert.Equal(t, wantError, gotError)
+}
