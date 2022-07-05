2f9f98dec55945cf5a5b7e3046d64e9dc924486e
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-05 12:12:20 +0100

    Add unit test for utils file (#53)

---
+++ utils/file_test.go
@@ -0,0 +1,30 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFileExt_WhenFilePathHasNoExtension(t *testing.T) {
+	fp := "test/file"
+
+	wantFileExt := ""
+	gotFileExt := utils.FileExt(fp)
+
+	assert.Equal(t, wantFileExt, gotFileExt)
+}
+
+func TestFileExt_WhenFilePathHasExtension(t *testing.T) {
+	fp := "test/file.go"
+
+	wantFileExt := ".go"
+	gotFileExt := utils.FileExt(fp)
+
+	assert.Equal(t, wantFileExt, gotFileExt)
+}
