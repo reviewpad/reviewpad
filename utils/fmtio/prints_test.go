19574fc1ac302aace0943cf4c58f6072a73575e2
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-05 12:17:51 +0100

    Add unit tests for utils fmtio (#54)

---
+++ utils/fmtio/prints_test.go
@@ -0,0 +1,26 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package fmtio_test
+
+import (
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/utils/fmtio"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSprintf(t *testing.T) {
+	wantStr := "[test] test value: TEST"
+	gotStr := fmtio.Sprintf("test", "test value: %v", "TEST")
+
+	assert.Equal(t, wantStr, gotStr)
+}
+
+func TestSprint(t *testing.T) {
+	wantStr := "[test] testing"
+	gotStr := fmtio.Sprint("test", "testing")
+
+	assert.Equal(t, wantStr, gotStr)
+}
