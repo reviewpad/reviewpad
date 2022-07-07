e21e0f292d41595dfb779ff6d7ce62f90743b91f
Cheila Alves <calves.200025@gmail.com>
2022-07-07 12:37:27 +0100

    Add unit tests for $createdAt function builtin

---
+++ plugins/aladino/functions/createdAt_test.go
@@ -0,0 +1,37 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var createdAt = plugins_aladino.PluginBuiltIns().Functions["createdAt"].Code
+
+func TestCreatedAt(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	wantCreatedAtTime, err := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", mockedEnv.GetPullRequest().GetCreatedAt().String())
+	if err != nil {
+		log.Fatalf("time.Parse failed: %v", err)
+	}
+	wantCreatedAt := aladino.BuildIntValue(int(wantCreatedAtTime.Unix()))
+
+	args := []aladino.Value{}
+	gotCreatedAt, err := createdAt(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantCreatedAt, gotCreatedAt)
+}
