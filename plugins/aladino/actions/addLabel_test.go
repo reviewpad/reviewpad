1132677c2ff3dd2a68f4eab0b657bba559d06214
Marcelo Sousa <601882+marcelosousa@users.noreply.github.com>
2022-07-07 16:27:06 +0100

    Use label key in environment to improve label actions (#72)
    * Add label map key name to aladino env
    
    * Remove check that is done by the typechecker
    
    * Update addLabel to use environment
    
    * Update removeLabel to use environment

--- plugins/aladino/actions/addLabel_test.go
+++ plugins/aladino/actions/addLabel_test.go
@@ -14,48 +14,12 @@ import (
 	"github.com/google/go-github/v42/github"
 	"github.com/migueleliasweb/go-github-mock/src/mock"
 	"github.com/reviewpad/reviewpad/v2/lang/aladino"
-	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
 	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
 	"github.com/stretchr/testify/assert"
 )
 
 var addLabel = plugins_aladino.PluginBuiltIns().Actions["addLabel"].Code
-
-func TestAddLabel_WhenArgumentIsInvalid(t *testing.T) {
-	mockedEnv, err := mocks_aladino.MockDefaultEnv()
-	if err != nil {
-		log.Fatalf("mockDefaultEnv failed: %v", err)
-	}
-
-	args := []aladino.Value{aladino.BuildIntValue(1)}
-	err = addLabel(mockedEnv, args)
-
-	assert.EqualError(t, err, "addLabel: expecting string argument, got IntValue")
-}
-
-func TestAddLabel_WhenGetLabelRequestFails(t *testing.T) {
-	failMessage := "GetLabelRequestFail"
-	mockedEnv, err := mocks_aladino.MockDefaultEnv(
-		mock.WithRequestMatchHandler(
-			mock.GetReposLabelsByOwnerByRepoByName,
-			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				mock.WriteError(
-					w,
-					http.StatusInternalServerError,
-					failMessage,
-				)
-			}),
-		),
-	)
-	if err != nil {
-		log.Fatalf("mockDefaultEnv failed: %v", err)
-	}
-
-	args := []aladino.Value{aladino.BuildStringValue("test")}
-	err = addLabel(mockedEnv, args)
-
-	assert.Equal(t, err.(*github.ErrorResponse).Message, failMessage)
-}
 
 func TestAddLabel_WhenAddLabelToIssueRequestFails(t *testing.T) {
 	label := "bug"
@@ -88,7 +52,7 @@ func TestAddLabel_WhenAddLabelToIssueRequestFails(t *testing.T) {
 	assert.Equal(t, err.(*github.ErrorResponse).Message, failMessage)
 }
 
-func TestAddLabel(t *testing.T) {
+func TestAddLabel_WhenLabelIsInEnvironment(t *testing.T) {
 	label := "bug"
 	wantLabels := []string{
 		label,
@@ -104,7 +68,43 @@ func TestAddLabel(t *testing.T) {
 			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				rawBody, _ := ioutil.ReadAll(r.Body)
 				body := []string{}
-				
+
+				json.Unmarshal(rawBody, &body)
+
+				gotLabels = body
+			}),
+		),
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+	internalLabelID := aladino.BuildInternalLabelID(label)
+	mockedEnv.GetRegisterMap()[internalLabelID] = aladino.BuildStringValue(label)
+
+	args := []aladino.Value{aladino.BuildStringValue(label)}
+	err = addLabel(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, wantLabels, gotLabels)
+}
+
+func TestAddLabel_WhenLabelIsNotInEnvironment(t *testing.T) {
+	label := "bug"
+	wantLabels := []string{
+		label,
+	}
+	gotLabels := []string{}
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatch(
+			mock.GetReposLabelsByOwnerByRepoByName,
+			&github.Label{},
+		),
+		mock.WithRequestMatchHandler(
+			mock.PostReposIssuesLabelsByOwnerByRepoByIssueNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				rawBody, _ := ioutil.ReadAll(r.Body)
+				body := []string{}
+
 				json.Unmarshal(rawBody, &body)
 
 				gotLabels = body
@@ -119,5 +119,5 @@ func TestAddLabel(t *testing.T) {
 	err = addLabel(mockedEnv, args)
 
 	assert.Nil(t, err)
-	assert.Equal(t, wantLabels, gotLabels)
+	assert.ElementsMatch(t, wantLabels, gotLabels)
 }
