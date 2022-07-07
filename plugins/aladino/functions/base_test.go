fd6676de86ee5a2ce3c229af219ca40309bc5f6b
Cheila Alves <calves.200025@gmail.com>
2022-07-07 11:09:54 +0100

    Add unit tests for $base function builtin

---
+++ plugins/aladino/functions/base_test.go
@@ -0,0 +1,32 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var base = plugins_aladino.PluginBuiltIns().Functions["base"].Code
+
+func TestBase(t *testing.T) {
+  mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+  wantBase := aladino.BuildStringValue(mockedEnv.GetPullRequest().GetBase().GetRef())
+
+  args := []aladino.Value{}
+  gotBase, err := base(mockedEnv, args)
+
+  assert.Nil(t, err)
+  assert.Equal(t, wantBase, gotBase)
+}
