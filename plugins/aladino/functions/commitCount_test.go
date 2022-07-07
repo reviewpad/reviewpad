8d8e557d382f8f6de624a7bb77a93363a275b628
Cheila Alves <calves.200025@gmail.com>
2022-07-07 17:18:28 +0100

    Mock pull request data in $commitCount unit tests

--- plugins/aladino/functions/commitCount_test.go
+++ plugins/aladino/functions/commitCount_test.go
@@ -6,8 +6,10 @@ package plugins_aladino_functions_test
 
 import (
 	"log"
+	"net/http"
 	"testing"
 
+	"github.com/migueleliasweb/go-github-mock/src/mock"
 	"github.com/reviewpad/reviewpad/v2/lang/aladino"
 	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
 	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
@@ -17,16 +19,27 @@ import (
 var commitCount = plugins_aladino.PluginBuiltIns().Functions["commitCount"].Code
 
 func TestCommitCount(t *testing.T) {
-  mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	totalCommits := 1
+	defaultPullRequestDetails := mocks_aladino.GetDefaultMockPullRequestDetails()
+	defaultPullRequestDetails.Commits = &totalCommits
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatchHandler(
+			// Overwrite default mock to pull request request details
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write(mock.MustMarshal(defaultPullRequestDetails))
+			}),
+		),
+	)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-  wantCommitCount := aladino.BuildIntValue(*mockedEnv.GetPullRequest().Commits)
+	wantCommitCount := aladino.BuildIntValue(*mockedEnv.GetPullRequest().Commits)
 
-  args := []aladino.Value{}
-  gotCommitCount, err := commitCount(mockedEnv, args)
+	args := []aladino.Value{}
+	gotCommitCount, err := commitCount(mockedEnv, args)
 
-  assert.Nil(t, err)
-  assert.Equal(t, wantCommitCount, gotCommitCount)
+	assert.Nil(t, err)
+	assert.Equal(t, wantCommitCount, gotCommitCount)
 }
