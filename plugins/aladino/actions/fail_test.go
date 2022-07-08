9b317031e35f0b7a479ff3bbb15de77e69308a46
Cheila Alves <calves.200025@gmail.com>
2022-07-08 14:39:34 +0100

    Add unit tests for $fail action builtin

---
+++ plugins/aladino/actions/fail_test.go
@@ -0,0 +1,29 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_actions_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var fail = plugins_aladino.PluginBuiltIns().Actions["fail"].Code
+
+func TestFail(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{}
+	err = fail(mockedEnv, args)
+
+	assert.EqualError(t, err, "action failed")
+}
