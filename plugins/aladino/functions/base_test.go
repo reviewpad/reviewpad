f5fc3106dc8dc793fde39569302d2e0547286138
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-11 16:26:11 +0100

    Add unit tests for `$base` function builtin (#77)

---
+++ plugins/aladino/functions/base_test.go
@@ -0,0 +1,54 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var base = plugins_aladino.PluginBuiltIns().Functions["base"].Code
+
+func TestBase(t *testing.T) {
+	baseRef := "master"
+	mockedPullRequest := mocks_aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		Base: &github.PullRequestBranch{
+            Repo: &github.Repository{
+                Owner: &github.User{
+                    Login: github.String("john"),
+                },
+                Name: github.String("default-mock-repo"),
+            },
+			Ref: github.String(baseRef),
+		},
+	})
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatchHandler(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write(mock.MustMarshal(mockedPullRequest))
+			}),
+		),
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	wantBase := aladino.BuildStringValue(baseRef)
+
+	args := []aladino.Value{}
+	gotBase, err := base(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantBase, gotBase, "it should get the pull request base reference")
+}
