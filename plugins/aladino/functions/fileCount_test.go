2127f4e140b1d5af26fb4532e91b33eefa445ef2
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-11 16:39:24 +0100

    Add unit tests for `$fileCount` function builtin (#83)

---
+++ plugins/aladino/functions/fileCount_test.go
@@ -0,0 +1,48 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var fileCount = plugins_aladino.PluginBuiltIns().Functions["fileCount"].Code
+
+func TestFileCount(t *testing.T) {
+	mockedPullRequestFileList := &[]*github.CommitFile{
+		{
+			Filename: github.String("default-mock-repo/file1.ts"),
+			Patch:    nil,
+		},
+	}
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatchHandler(
+			mock.GetReposPullsFilesByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write(mock.MustMarshal(mockedPullRequestFileList))
+			}),
+		),
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	wantFileCount := aladino.BuildIntValue(len(*mockedPullRequestFileList))
+
+	args := []aladino.Value{}
+	gotFileCount, err := fileCount(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantFileCount, gotFileCount, "action should count the total pull request files")
+}
