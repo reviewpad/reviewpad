2414c4c866b9dfa735b0b24c5f894ff996bf187c
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-11 16:34:40 +0100

    Add unit tests for `$description` function builtin (#82)

---
+++ plugins/aladino/functions/description_test.go
@@ -0,0 +1,46 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var description = plugins_aladino.PluginBuiltIns().Functions["description"].Code
+
+func TestDescription(t *testing.T) {
+    prDescription := "Please pull these awesome changes in!"
+	mockedPullRequest := mocks_aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		Body: github.String(prDescription),
+	})
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+        mock.WithRequestMatchHandler(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write(mock.MustMarshal(mockedPullRequest))
+			}),
+		),
+    )
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	wantDescription := aladino.BuildStringValue(prDescription)
+
+	args := []aladino.Value{}
+	gotDescription, err := description(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantDescription, gotDescription)
+}
