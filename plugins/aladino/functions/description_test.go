6434beef1ad2d974e2a28abbf0a9eb4cfbdc690e
Cheila Alves <calves.200025@gmail.com>
2022-07-11 11:28:52 +0100

    Apply GetDefaultMockPullRequestDetailsWith to description unit tests

--- plugins/aladino/functions/description_test.go
+++ plugins/aladino/functions/description_test.go
@@ -6,8 +6,11 @@ package plugins_aladino_functions_test
 
 import (
 	"log"
+	"net/http"
 	"testing"
 
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
 	"github.com/reviewpad/reviewpad/v2/lang/aladino"
 	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
 	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
@@ -17,16 +20,27 @@ import (
 var description = plugins_aladino.PluginBuiltIns().Functions["description"].Code
 
 func TestDescription(t *testing.T) {
-  mockedEnv, err := mocks_aladino.MockDefaultEnv()
+    prDescription := "Please pull these awesome changes in!"
+	mockedPullRequest := mocks_aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		Body: github.String(prDescription),
+	})
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+        mock.WithRequestMatchHandler(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write(mock.MustMarshal(mockedPullRequest))
+			}),
+		),
+    )
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-  wantDescription := aladino.BuildStringValue(mockedEnv.GetPullRequest().GetBody())
+	wantDescription := aladino.BuildStringValue(prDescription)
 
-  args := []aladino.Value{}
+	args := []aladino.Value{}
 	gotDescription, err := description(mockedEnv, args)
 
-  assert.Nil(t, err)
-  assert.Equal(t, wantDescription, gotDescription)
+	assert.Nil(t, err)
+	assert.Equal(t, wantDescription, gotDescription)
 }
