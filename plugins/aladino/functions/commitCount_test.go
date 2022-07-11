6ec2a9acb2886378e469a4681694f84df2dd4b17
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-11 16:30:14 +0100

    Add unit tests for `$commitCount` function builtin (#78)

---
+++ plugins/aladino/functions/commitCount_test.go
@@ -0,0 +1,46 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var commitCount = plugins_aladino.PluginBuiltIns().Functions["commitCount"].Code
+
+func TestCommitCount(t *testing.T) {
+	totalCommits := 1
+	mockedPullRequest := mocks_aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		Commits: &totalCommits,
+	})
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		mock.WithRequestMatchHandler(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write(mock.MustMarshal(mockedPullRequest))
+			}),
+		),
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	wantCommitCount := aladino.BuildIntValue(totalCommits)
+
+	args := []aladino.Value{}
+	gotCommitCount, err := commitCount(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantCommitCount, gotCommitCount, "it should get the pull request commit count")
+}
