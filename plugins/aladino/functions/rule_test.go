8f6be467a647b119997df9c5a9dba9b67e3dff97
Cheila Alves <calves.200025@gmail.com>
2022-07-12 12:52:22 +0100

    Add $rule function unit tests

---
+++ plugins/aladino/functions/rule_test.go
@@ -0,0 +1,87 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var rule = plugins_aladino.PluginBuiltIns().Functions["rule"].Code
+
+func TestRule_WhenRuleIsAbsent(t *testing.T) {
+	ruleName := "is-absent"
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{aladino.BuildStringValue(ruleName)}
+	gotVal, err := rule(mockedEnv, args)
+
+	assert.Nil(t, gotVal)
+	assert.EqualError(t, err, fmt.Sprintf("$rule: no rule with name %v in state %+q", ruleName, mockedEnv.GetRegisterMap()))
+}
+
+func TestRule_WhenRuleIsInvalid(t *testing.T) {
+	ruleName := "is-invalid"
+	internalRuleName := fmt.Sprintf("@rule:%v", ruleName)
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	mockedEnv.GetRegisterMap()[internalRuleName] = aladino.BuildStringValue("1 == \"a\"")
+
+	args := []aladino.Value{aladino.BuildStringValue(ruleName)}
+	gotVal, err := rule(mockedEnv, args)
+
+	assert.Nil(t, gotVal)
+	assert.EqualError(t, err, "type inference failed")
+}
+
+func TestRule_WhenRuleIsTrue(t *testing.T) {
+	ruleName := "tautology"
+	internalRuleName := fmt.Sprintf("@rule:%v", ruleName)
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	mockedEnv.GetRegisterMap()[internalRuleName] = aladino.BuildStringValue("1 == 1")
+
+	args := []aladino.Value{aladino.BuildStringValue(ruleName)}
+	gotVal, err := rule(mockedEnv, args)
+
+	wantVal := aladino.BuildBoolValue(true)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
+
+func TestRule_WhenRuleIsFalse(t *testing.T) {
+	ruleName := "is-false-premise"
+	internalRuleName := fmt.Sprintf("@rule:%v", ruleName)
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	mockedEnv.GetRegisterMap()[internalRuleName] = aladino.BuildStringValue("1 == 2")
+
+	args := []aladino.Value{aladino.BuildStringValue(ruleName)}
+	gotVal, err := rule(mockedEnv, args)
+
+	wantVal := aladino.BuildBoolValue(false)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
