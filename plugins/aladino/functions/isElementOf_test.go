c8bdf6c82ab20ab7b7841b2a700b2fd5e5d1cf44
Cheila Alves <calves.200025@gmail.com>
2022-07-12 11:00:28 +0100

    Add $isElementOf function unit tests

---
+++ plugins/aladino/functions/isElementOf_test.go
@@ -0,0 +1,59 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var isElementOf = plugins_aladino.PluginBuiltIns().Functions["isElementOf"].Code
+
+func TestIsElementOf_WhenTrue(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{
+        aladino.BuildStringValue("elemA"),
+		aladino.BuildArrayValue([]aladino.Value{
+			aladino.BuildStringValue("elemA"),
+			aladino.BuildStringValue("elemB"),
+		}),
+	}
+	gotVal, err := isElementOf(mockedEnv, args)
+
+	wantVal := aladino.BuildBoolValue(true)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
+
+func TestIsElementOf_WhenFalse(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv()
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{
+        aladino.BuildStringValue("elemA"),
+		aladino.BuildArrayValue([]aladino.Value{
+			aladino.BuildStringValue("elemB"),
+			aladino.BuildStringValue("elemC"),
+		}),
+	}
+	gotVal, err := isElementOf(mockedEnv, args)
+
+	wantVal := aladino.BuildBoolValue(false)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
