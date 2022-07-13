1ec671e7155eb2aa8c2d98b7c7bb03e8190ecc55
Cheila Alves <calves.200025@gmail.com>
2022-07-13 12:00:20 +0100

    chore: change mocked pull request in $isDraft tests

--- plugins/aladino/functions/isDraft_test.go
+++ plugins/aladino/functions/isDraft_test.go
@@ -6,9 +6,11 @@ package plugins_aladino_functions_test
 
 import (
 	"log"
+	"net/http"
 	"testing"
 
 	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
 	"github.com/reviewpad/reviewpad/v2/lang/aladino"
 	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
 	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
@@ -18,36 +20,57 @@ import (
 var isDraft = plugins_aladino.PluginBuiltIns().Functions["isDraft"].Code
 
 func TestIsDraft_WhenTrue(t *testing.T) {
-    mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	mockedPullRequest := mocks_aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		Draft: github.Bool(true),
+	})
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+        []mock.MockBackendOption{
+			mock.WithRequestMatchHandler(
+				mock.GetReposPullsByOwnerByRepoByPullNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Write(mock.MustMarshal(mockedPullRequest))
+				}),
+			),
+		},
+		nil,
+    )
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    mockedEnv.GetPullRequest().Draft = github.Bool(true)
-
 	args := []aladino.Value{}
 	gotVal, err := isDraft(mockedEnv, args)
 
-    wantVal := aladino.BuildBoolValue(true)
+	wantVal := aladino.BuildBoolValue(true)
 
 	assert.Nil(t, err)
 	assert.Equal(t, wantVal, gotVal, "pull request should be in draft")
 }
 
 func TestIsDraft_WhenFalse(t *testing.T) {
-    mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	mockedPullRequest := mocks_aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		Draft: github.Bool(false),
+	})
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		[]mock.MockBackendOption{
+			mock.WithRequestMatchHandler(
+				mock.GetReposPullsByOwnerByRepoByPullNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Write(mock.MustMarshal(mockedPullRequest))
+				}),
+			),
+		},
+		nil,
+	)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    mockedEnv.GetPullRequest().Draft = github.Bool(false)
-
 	args := []aladino.Value{}
 	gotVal, err := isDraft(mockedEnv, args)
 
-    wantVal := aladino.BuildBoolValue(false)
+	wantVal := aladino.BuildBoolValue(false)
 
 	assert.Nil(t, err)
 	assert.Equal(t, wantVal, gotVal, "pull request should not be in draft")
 }
-
