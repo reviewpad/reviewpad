92ac0ea6b0947229f9c6f7c4dfe6c0486d72b75f
Cheila Alves <calves.200025@gmail.com>
2022-07-13 10:39:02 +0100

    chore: apply MockDefaultEnv changes to $head tests

--- plugins/aladino/functions/head_test.go
+++ plugins/aladino/functions/head_test.go
@@ -33,12 +33,15 @@ func TestHead(t *testing.T) {
 		},
 	})
 	mockedEnv, err := mocks_aladino.MockDefaultEnv(
-		mock.WithRequestMatchHandler(
-			mock.GetReposPullsByOwnerByRepoByPullNumber,
-			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				w.Write(mock.MustMarshal(mockedPullRequest))
-			}),
-		),
+		[]mock.MockBackendOption{
+			mock.WithRequestMatchHandler(
+				mock.GetReposPullsByOwnerByRepoByPullNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Write(mock.MustMarshal(mockedPullRequest))
+				}),
+			),
+		},
+		nil,
 	)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
