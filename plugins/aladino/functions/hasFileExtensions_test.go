851b102a29d833e2850c13790d60136a9f737823
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-13 10:52:50 +0100

    test: `$hasFileExtensions` function builtin unit tests (#95)

---
+++ plugins/aladino/functions/hasFileExtensions_test.go
@@ -0,0 +1,84 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v2/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var hasFileExtensions = plugins_aladino.PluginBuiltIns().Functions["hasFileExtensions"].Code
+
+func TestHasFileExtensions_WhenFalse(t *testing.T) {
+	defaultMockPrFileName := "default-mock-repo/file1.ts"
+	mockedPullRequestFileList := &[]*github.CommitFile{
+		{
+			Filename: github.String(defaultMockPrFileName),
+			Patch:    nil,
+		},
+	}
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		[]mock.MockBackendOption{
+			mock.WithRequestMatchHandler(
+				mock.GetReposPullsFilesByOwnerByRepoByPullNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Write(mock.MustMarshal(mockedPullRequestFileList))
+				}),
+			),
+		},
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue(".md")})}
+	gotVal, err := hasFileExtensions(mockedEnv, args)
+
+	wantVal := aladino.BuildBoolValue(false)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
+
+func TestHasFileExtensions_WhenTrue(t *testing.T) {
+	defaultMockPrFileName := "default-mock-repo/file1.ts"
+	mockedPullRequestFileList := &[]*github.CommitFile{
+		{
+			Filename: github.String(defaultMockPrFileName),
+			Patch:    nil,
+		},
+	}
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(
+		[]mock.MockBackendOption{
+			mock.WithRequestMatchHandler(
+				mock.GetReposPullsFilesByOwnerByRepoByPullNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Write(mock.MustMarshal(mockedPullRequestFileList))
+				}),
+			),
+		},
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	args := []aladino.Value{aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue(".ts")})}
+	gotVal, err := hasFileExtensions(mockedEnv, args)
+
+	wantVal := aladino.BuildBoolValue(true)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
