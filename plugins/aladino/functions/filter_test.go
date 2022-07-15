dda05125549d8002e413e0483eae702826888c92
Cheila Alves <calves.200025@gmail.com>
2022-07-15 11:29:42 +0100

    chore: add mockedIntValue in TestFilter

--- plugins/aladino/functions/filter_test.go
+++ plugins/aladino/functions/filter_test.go
@@ -17,21 +17,22 @@ import (
 var filter = plugins_aladino.PluginBuiltIns().Functions["filter"].Code
 
 func TestFilter(t *testing.T) {
+	mockedIntValue := aladino.BuildIntValue(1)
 	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
 	args := []aladino.Value{
-		aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue("1"), aladino.BuildIntValue(1)}),
+		aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue("1"), mockedIntValue}),
 		aladino.BuildFunctionValue(func(args []aladino.Value) aladino.Value {
 			return aladino.BuildBoolValue(args[0].HasKindOf(aladino.INT_VALUE))
 		}),
 	}
 	gotElems, err := filter(mockedEnv, args)
 
-	wantElems := aladino.BuildArrayValue([]aladino.Value{aladino.BuildIntValue(1)})
+	wantElems := aladino.BuildArrayValue([]aladino.Value{mockedIntValue})
 
-  assert.Nil(t, err)
-  assert.Equal(t, wantElems, gotElems)
+	assert.Nil(t, err)
+	assert.Equal(t, wantElems, gotElems)
 }
