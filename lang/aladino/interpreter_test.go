c752fa7287fcb00809620fe2718e5cb12584186e
Cheila Alves <calves.200025@gmail.com>
2022-07-19 16:19:00 +0100

    test: add unit tests for lang/aladino interpreter BuildInternalLabelID

--- lang/aladino/interpreter_test.go
+++ lang/aladino/interpreter_test.go
@@ -3,3 +3,21 @@
 // found in the LICENSE file.
 
 package aladino_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildInternalLabelID(t *testing.T) {
+	labelID := "label_id"
+
+	wantVal := fmt.Sprintf("@label:%v", labelID)
+
+	gotVal := aladino.BuildInternalLabelID(labelID)
+
+	assert.Equal(t, wantVal, gotVal)
+}
