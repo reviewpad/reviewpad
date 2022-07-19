1757046a83dcd7d8525bed819b22863b2a28b199
Cheila Alves <calves.200025@gmail.com>
2022-07-19 17:04:12 +0100

    test: add unit tests for lang/aladino interpreter EvalExpr

--- lang/aladino/interpreter_test.go
+++ lang/aladino/interpreter_test.go
@@ -3,3 +3,76 @@
 // found in the LICENSE file.
 
 package aladino_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvalExpr_WhenParseFails(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	gotVal, err := aladino.EvalExpr(mockedEnv, "", "1 ==")
+
+	assert.False(t, gotVal)
+	assert.EqualError(t, err, "parse error: failed to build AST on input 1 ==")
+}
+
+func TestEvalExpr_WhenTypeInferenceFails(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	gotVal, err := aladino.EvalExpr(mockedEnv, "", "1 == \"a\"")
+
+	assert.False(t, gotVal)
+	assert.EqualError(t, err, "type inference failed")
+}
+
+func TestEvalExpr_WhenExprIsNotBoolType(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	gotVal, err := aladino.EvalExpr(mockedEnv, "", "1")
+
+	assert.False(t, gotVal)
+	assert.EqualError(t, err, "expression 1 is not a condition")
+}
+
+func TestEvalExpr(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	gotVal, err := aladino.EvalExpr(mockedEnv, "", "1 == 1")
+
+	assert.Nil(t, err)
+	assert.True(t, gotVal)
+}
+
+func TestEvalExpr_OnInterpreter(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	mockedInterpreter := &aladino.Interpreter{
+		Env: mockedEnv,
+	}
+
+	gotVal, err := mockedInterpreter.EvalExpr("", "1 == 1")
+
+	assert.Nil(t, err)
+	assert.True(t, gotVal)
+}
