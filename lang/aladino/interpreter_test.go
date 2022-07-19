cd73175797891cbb64bc8c9988eff510118a9b36
Cheila Alves <calves.200025@gmail.com>
2022-07-19 16:31:25 +0100

    test: add unit tests for lang/aladino interpreter ProcessLabel

--- lang/aladino/interpreter_test.go
+++ lang/aladino/interpreter_test.go
@@ -3,3 +3,36 @@
 // found in the LICENSE file.
 
 package aladino_test
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessLabel(t *testing.T) {
+	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	mockedInterpreter := &aladino.Interpreter{
+		Env: mockedEnv,
+	}
+
+	labelID := "label_id"
+	labelName := "label_name"
+	err = mockedInterpreter.ProcessLabel(labelID, labelName)
+
+	internalLabelID := fmt.Sprintf("@label:%v", labelID)
+	gotVal := mockedEnv.GetRegisterMap()[internalLabelID]
+
+	wantVal := aladino.BuildStringValue(labelName)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantVal, gotVal)
+}
