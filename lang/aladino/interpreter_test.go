4e6b18da0be38bff857690ef4fac82683f2b6294
Cheila Alves <calves.200025@gmail.com>
2022-07-19 16:34:12 +0100

    test: add unit tests for lang/aladino interpreter BuildInternalRuleName

--- lang/aladino/interpreter_test.go
+++ lang/aladino/interpreter_test.go
@@ -3,3 +3,21 @@
 // found in the LICENSE file.
 
 package aladino_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v2/lang/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildInternalRuleName(t *testing.T) {
+	ruleName := "rule_name"
+
+	wantVal := fmt.Sprintf("@rule:%v", ruleName)
+
+	gotVal := aladino.BuildInternalRuleName(ruleName)
+
+	assert.Equal(t, wantVal, gotVal)
+}
