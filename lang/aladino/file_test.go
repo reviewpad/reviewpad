c1d9cb8b98adbe5a95176b7efbb863d3bbd272a4
Cheila Alves <calves.200025@gmail.com>
2022-07-22 10:43:43 +0100

    chore: change mocks_aladino imports to aladino

--- lang/aladino/file_test.go
+++ lang/aladino/file_test.go
@@ -6,37 +6,24 @@ package aladino_test
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"testing"
 
 	"github.com/google/go-github/v42/github"
-	"github.com/migueleliasweb/go-github-mock/src/mock"
-	"github.com/reviewpad/reviewpad/v2/lang/aladino"
-	mocks_aladino "github.com/reviewpad/reviewpad/v2/mocks/aladino"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
 	"github.com/stretchr/testify/assert"
 )
 
-// mocks_aladino.MockDefaultEnv calls aladino.NewFile upon creating the new eval env
+// aladino.MockDefaultEnv calls aladino.NewFile upon creating the new eval env
 func TestNewFile_WhenErrorInFilePatch(t *testing.T) {
 	fileName := "default-mock-repo/file1.ts"
 	mockedFile := &github.CommitFile{
 		Patch:    github.String("@@"),
 		Filename: github.String(fileName),
 	}
-	mockedPullRequestFileList := &[]*github.CommitFile{mockedFile}
-	_, err := mocks_aladino.MockDefaultEnv(
-		[]mock.MockBackendOption{
-			mock.WithRequestMatchHandler(
-				mock.GetReposPullsFilesByOwnerByRepoByPullNumber,
-				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-					w.Write(mock.MustMarshal(mockedPullRequestFileList))
-				}),
-			),
-		},
-		nil,
-	)
 
+	gotFile, err := aladino.NewFile(mockedFile)
+
+	assert.Nil(t, gotFile)
 	assert.EqualError(t, err, fmt.Sprintf("error in file patch %s: error in chunk lines parsing (1): missing lines info: @@\npatch: @@", fileName))
 }
 
@@ -46,40 +33,25 @@ func TestNewFile(t *testing.T) {
 		Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
 		Filename: github.String(fileName),
 	}
-	mockedPullRequestFileList := &[]*github.CommitFile{mockedFile}
-	mockedEnv, err := mocks_aladino.MockDefaultEnv(
-		[]mock.MockBackendOption{
-			mock.WithRequestMatchHandler(
-				mock.GetReposPullsFilesByOwnerByRepoByPullNumber,
-				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-					w.Write(mock.MustMarshal(mockedPullRequestFileList))
-				}),
-			),
-		},
-		nil,
-	)
-	if err != nil {
-		log.Fatalf("mockDefaultEnv failed: %v", err)
-	}
 
 	wantFile := &aladino.File{
 		Repr: mockedFile,
 	}
 	wantFile.AppendToDiff(false, 2, 2, 2, 3, " func previous() {", " func new() {\n")
 
-	gotFile := mockedEnv.GetPatch()[fileName]
+	gotFile, err := aladino.NewFile(mockedFile)
 
+	assert.Nil(t, err)
 	assert.Equal(t, wantFile, gotFile)
 }
 
 func TestQuery_WhenCompileFails(t *testing.T) {
 	fileName := "default-mock-repo/file1.ts"
-	mockedCommitFile := &github.CommitFile{
-		Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
-		Filename: github.String(fileName),
-	}
 	mockedFile := &aladino.File{
-		Repr: mockedCommitFile,
+		Repr: &github.CommitFile{
+			Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
+			Filename: github.String(fileName),
+		},
 	}
 	mockedFile.AppendToDiff(false, 2, 2, 2, 3, " func previous() {", " func new() {\n")
 
@@ -91,12 +63,11 @@ func TestQuery_WhenCompileFails(t *testing.T) {
 
 func TestQuery_WhenFound(t *testing.T) {
 	fileName := "default-mock-repo/file1.ts"
-	mockedCommitFile := &github.CommitFile{
-		Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
-		Filename: github.String(fileName),
-	}
 	mockedFile := &aladino.File{
-		Repr: mockedCommitFile,
+		Repr: &github.CommitFile{
+			Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
+			Filename: github.String(fileName),
+		},
 	}
 	mockedFile.AppendToDiff(false, 2, 2, 2, 3, " func previous() {", " func new() {\n")
 
@@ -108,12 +79,11 @@ func TestQuery_WhenFound(t *testing.T) {
 
 func TestQuery_WhenNotFound(t *testing.T) {
 	fileName := "default-mock-repo/file1.ts"
-	mockedCommitFile := &github.CommitFile{
-		Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
-		Filename: github.String(fileName),
-	}
 	mockedFile := &aladino.File{
-		Repr: mockedCommitFile,
+		Repr: &github.CommitFile{
+			Patch:    github.String("@@ -2,9 +2,11 @@ package main\n- func previous() {\n+ func new() {\n+\nreturn"),
+			Filename: github.String(fileName),
+		},
 	}
 	mockedFile.AppendToDiff(false, 2, 2, 2, 3, " func previous() {", " func new() {\n")
 
