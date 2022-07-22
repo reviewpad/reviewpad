f05c3849b962f92cf988fa19a4791c74baf55d34
Cheila Alves <calves.200025@gmail.com>
2022-07-22 11:18:42 +0100

    test: add unit tests for lang aladino exec action

--- lang/aladino/exec_internal_test.go
+++ lang/aladino/exec_internal_test.go
@@ -17,49 +17,113 @@ func TestTypeCheckExec_WhenTypeInferenceFails(t *testing.T) {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    expr, err := Parse("$addLabel(1)")
-    if err != nil {
+	expr, err := Parse("$emptyAction(1)")
+	if err != nil {
 		log.Fatalf("parse failed: %v", err)
 	}
 
-    execExpr, err := TypeCheckExec(mockedEnv, expr)
+	execExpr, err := TypeCheckExec(mockedEnv, expr)
 
-    assert.Nil(t, execExpr)
-    assert.EqualError(t, err, "type inference failed: mismatch in arg types on addLabel")
+	assert.Nil(t, execExpr)
+	assert.EqualError(t, err, "type inference failed: mismatch in arg types on emptyAction")
 }
 
 func TestTypeCheck(t *testing.T) {
-    mockedEnv, err := MockDefaultEnv(nil, nil)
+	mockedEnv, err := MockDefaultEnv(nil, nil)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    expr, err := Parse("$addLabel(\"label\")")
-    if err != nil {
+	expr, err := Parse("$emptyAction()")
+	if err != nil {
 		log.Fatalf("parse failed: %v", err)
 	}
 
-    gotExecExpr, err := TypeCheckExec(mockedEnv, expr)
+	gotExecExpr, err := TypeCheckExec(mockedEnv, expr)
 
-    wantExecExpr := BuildFunctionCall(BuildVariable("addLabel"), []Expr{BuildStringConst("label")})
+	wantExecExpr := BuildFunctionCall(BuildVariable("emptyAction"), []Expr{})
 
-    assert.Nil(t, err)
-    assert.Equal(t, wantExecExpr, gotExecExpr)
+	assert.Nil(t, err)
+	assert.Equal(t, wantExecExpr, gotExecExpr)
 }
 
 func TestTypeCheck_WhenExprIsNotFunctionCall(t *testing.T) {
-    mockedEnv, err := MockDefaultEnv(nil, nil)
+	mockedEnv, err := MockDefaultEnv(nil, nil)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    expr, err := Parse("\"not a function call\"")
-    if err != nil {
+	expr, err := Parse("\"not a function call\"")
+	if err != nil {
 		log.Fatalf("parse failed: %v", err)
 	}
 
-    gotExecExpr, err := TypeCheckExec(mockedEnv, expr)
+	gotExecExpr, err := TypeCheckExec(mockedEnv, expr)
 
-    assert.Nil(t, gotExecExpr)
-    assert.EqualError(t, err, "typecheckexec: StringConst")
+	assert.Nil(t, gotExecExpr)
+	assert.EqualError(t, err, "typecheckexec: StringConst")
+}
+
+func TestExec_WhenFunctionArgsEvalFails(t *testing.T) {
+	mockedEnv, err := MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	fc := &FunctionCall{
+		name: BuildVariable("invalidCmpOp"),
+		arguments: []Expr{
+			BuildEqOp(BuildIntConst(0), BuildStringConst("0")),
+		},
+	}
+
+	err = fc.exec(mockedEnv)
+
+	assert.EqualError(t, err, "eval: left and right operand have different kinds")
+}
+
+func TestExec_WhenActionBuiltInNonExisting(t *testing.T) {
+	mockedEnv, err := MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+	// make sure "tautology" action builtin doesn't exist in mocked environment
+	delete(mockedEnv.GetBuiltIns().Actions, "tautology")
+
+	fc := &FunctionCall{
+		name: BuildVariable("tautology"),
+		arguments: []Expr{
+			BuildEqOp(BuildIntConst(1), BuildIntConst(1)),
+		},
+	}
+
+	err = fc.exec(mockedEnv)
+
+	assert.EqualError(t, err, "exec: tautology not found. are you sure this is a built-in function?")
+}
+
+func TestExec(t *testing.T) {
+	mockedEnv, err := MockDefaultEnv(nil, nil)
+	if err != nil {
+		log.Fatalf("mockDefaultEnv failed: %v", err)
+	}
+
+    fcName := "emptyAction"
+
+	mockedEnv.GetBuiltIns().Actions["emptyAction"] = &BuiltInAction{
+		Type: BuildFunctionType([]Type{}, nil),
+		Code: func(e Env, args []Value) error {
+			return nil
+		},
+	}
+
+	fc := &FunctionCall{
+		name:      BuildVariable(fcName),
+		arguments: []Expr{},
+	}
+
+	err = fc.exec(mockedEnv)
+
+	assert.Nil(t, err)
 }
