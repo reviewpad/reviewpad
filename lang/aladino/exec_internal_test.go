d3a7bab66b90c898d7715889158a30d83f11904d
Cheila Alves <calves.200025@gmail.com>
2022-07-22 10:48:25 +0100

    chore: change mocks_aladino imports to aladino

--- lang/aladino/exec_internal_test.go
+++ lang/aladino/exec_internal_test.go
@@ -2,65 +2,63 @@
 // Use of this source code is governed by a license that can be
 // found in the LICENSE file.
 
-package aladino_test
+package aladino
 
 import (
 	"log"
 	"testing"
 
-	"github.com/reviewpad/reviewpad/v3/lang/aladino"
-	mocks_aladino "github.com/reviewpad/reviewpad/v3/mocks/aladino"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTypeCheckExec_WhenTypeInferenceFails(t *testing.T) {
-	mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+	mockedEnv, err := MockDefaultEnv(nil, nil)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    expr, err := aladino.Parse("$addLabel(1)")
+    expr, err := Parse("$addLabel(1)")
     if err != nil {
 		log.Fatalf("parse failed: %v", err)
 	}
 
-    execExpr, err := aladino.TypeCheckExec(mockedEnv, expr)
+    execExpr, err := TypeCheckExec(mockedEnv, expr)
 
     assert.Nil(t, execExpr)
     assert.EqualError(t, err, "type inference failed: mismatch in arg types on addLabel")
 }
 
 func TestTypeCheck(t *testing.T) {
-    mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+    mockedEnv, err := MockDefaultEnv(nil, nil)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    expr, err := aladino.Parse("$addLabel(\"label\")")
+    expr, err := Parse("$addLabel(\"label\")")
     if err != nil {
 		log.Fatalf("parse failed: %v", err)
 	}
 
-    gotExecExpr, err := aladino.TypeCheckExec(mockedEnv, expr)
+    gotExecExpr, err := TypeCheckExec(mockedEnv, expr)
 
-    wantExecExpr := aladino.BuildFunctionCall(aladino.BuildVariable("addLabel"), []aladino.Expr{aladino.BuildStringConst("label")})
+    wantExecExpr := BuildFunctionCall(BuildVariable("addLabel"), []Expr{BuildStringConst("label")})
 
     assert.Nil(t, err)
     assert.Equal(t, wantExecExpr, gotExecExpr)
 }
 
 func TestTypeCheck_WhenExprIsNotFunctionCall(t *testing.T) {
-    mockedEnv, err := mocks_aladino.MockDefaultEnv(nil, nil)
+    mockedEnv, err := MockDefaultEnv(nil, nil)
 	if err != nil {
 		log.Fatalf("mockDefaultEnv failed: %v", err)
 	}
 
-    expr, err := aladino.Parse("\"not a function call\"")
+    expr, err := Parse("\"not a function call\"")
     if err != nil {
 		log.Fatalf("parse failed: %v", err)
 	}
 
-    gotExecExpr, err := aladino.TypeCheckExec(mockedEnv, expr)
+    gotExecExpr, err := TypeCheckExec(mockedEnv, expr)
 
     assert.Nil(t, gotExecExpr)
     assert.EqualError(t, err, "typecheckexec: StringConst")
