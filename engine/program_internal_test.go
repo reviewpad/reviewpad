dd2109d61eb7c5720ce9bc125df75c628240c58d
Cheila Alves <calves.200025@gmail.com>
2022-07-26 16:46:23 +0100

    test: add unit tests for engine program

---
+++ engine/program_internal_test.go
@@ -0,0 +1,69 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAppend(t *testing.T) {
+	workflow := PadWorkflow{
+		Name:        "test-workflow-A",
+		Description: "Testing workflow",
+		AlwaysRun:   true,
+		Rules: []PadWorkflowRule{
+			{Rule: "test-rule-A"},
+		},
+		Actions: []string{"$actionA()"},
+	}
+	actions := []string{"$actionB()"}
+	rules := []PadWorkflowRule{
+		{Rule: "test-rule-B"},
+	}
+	program := &Program{
+		Statements: []*Statement{
+			{
+				Code: "$actionC()",
+				Metadata: &Metadata{
+					Workflow: PadWorkflow{
+						Name: "test-workflow-C",
+					},
+					TriggeredBy: []PadWorkflowRule{
+						{Rule: "test-rule-C"},
+					},
+				},
+			},
+		},
+	}
+
+	wantProgram := &Program{
+		Statements: []*Statement{
+			{
+				Code: "$actionC()",
+				Metadata: &Metadata{
+					Workflow: PadWorkflow{
+						Name: "test-workflow-C",
+					},
+					TriggeredBy: []PadWorkflowRule{
+						{Rule: "test-rule-C"},
+					},
+				},
+			},
+			{
+				Code: "$actionB()",
+				Metadata: &Metadata{
+					Workflow:    workflow,
+					TriggeredBy: rules,
+				},
+			},
+		},
+	}
+
+	program.append(actions, workflow, rules)
+
+	assert.Equal(t, wantProgram, program)
+}
