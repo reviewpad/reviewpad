de9f627b3c01fde6682f11be8c09610e208333e4
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-07-27 11:20:22 +0100

    test: add unit tests for engine env & mocks for engine (#171)

---
+++ engine/env_test.go
@@ -0,0 +1,78 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package engine_test
+
+import (
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/reviewpad/reviewpad/v3/engine"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewEvalEnv(t *testing.T) {
+	ctx := engine.DefaultMockCtx
+	client := engine.MockGithubClient(nil)
+	collector := engine.DefaultMockCollector
+	mockedPullRequest := engine.GetDefaultMockPullRequestDetails()
+	fileName := "default-mock-repo/file1.ts"
+	patch := `@@ -2,9 +2,11 @@ package main
+- func previous1() {
++ func new1() {
++
+return`
+	
+    mockedFile := &aladino.File{
+		Repr: &github.CommitFile{
+			Filename: github.String(fileName),
+			Patch:    github.String(patch),
+		},
+	}
+	mockedFile.AppendToDiff(false, 2, 2, 2, 3, " func previous1() {", " func new1() {\n")
+	mockedPatch := aladino.Patch{
+		fileName: mockedFile,
+	}
+
+	mockedAladinoInterpreter := &aladino.Interpreter{
+		Env: &aladino.BaseEnv{
+			Ctx:          ctx,
+			Client:       client,
+			ClientGQL:    nil,
+			Collector:    collector,
+			PullRequest:  mockedPullRequest,
+			Patch:        mockedPatch,
+			RegisterMap:  aladino.RegisterMap(make(map[string]aladino.Value)),
+			BuiltIns:     aladino.MockBuiltIns(),
+			Report:       &aladino.Report{WorkflowDetails: make(map[string]aladino.ReportWorkflowDetails)},
+			EventPayload: engine.DefaultMockEventPayload,
+		},
+	}
+
+	wantEnv := &engine.Env{
+		Ctx:          ctx,
+		DryRun:       false,
+		Client:       client,
+		ClientGQL:    nil,
+		Collector:    collector,
+		PullRequest:  mockedPullRequest,
+		EventPayload: engine.DefaultMockEventPayload,
+		Interpreter:  mockedAladinoInterpreter,
+	}
+
+	gotEnv, err := engine.NewEvalEnv(
+		ctx,
+		false,
+		client,
+		nil,
+		collector,
+		mockedPullRequest,
+		engine.DefaultMockEventPayload,
+		mockedAladinoInterpreter,
+	)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantEnv, gotEnv)
+}
---
+++ engine/mocks.go
@@ -0,0 +1,51 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package engine
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v3/collector"
+)
+
+// Use only for tests
+const defaultMockPrID = 1234
+const defaultMockPrNum = 6
+
+// Use only for tests
+var DefaultMockCtx = context.Background()
+var DefaultMockCollector = collector.NewCollector("", "")
+var DefaultMockEventPayload = &github.CheckRunEvent{}
+
+func GetDefaultMockPullRequestDetails() *github.PullRequest {
+	prNum := defaultMockPrNum
+	prId := int64(defaultMockPrID)
+	prDate := time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+
+	return &github.PullRequest{
+		ID:        &prId,
+		User:      &github.User{Login: github.String("john")},
+		Title:     github.String("Amazing new feature"),
+		Body:      github.String("Please pull these awesome changes in!"),
+		CreatedAt: &prDate,
+		Number:    github.Int(prNum),
+	}
+}
+
+func mockDefaultHttpClient(clientOptions []mock.MockBackendOption) *http.Client {
+	return mockHttpClientWith(clientOptions...)
+}
+
+func mockHttpClientWith(clientOptions ...mock.MockBackendOption) *http.Client {
+	return mock.NewMockedHTTPClient(clientOptions...)
+}
+
+func MockGithubClient(clientOptions []mock.MockBackendOption) *github.Client {
+	return github.NewClient(mockDefaultHttpClient(clientOptions))
+}
