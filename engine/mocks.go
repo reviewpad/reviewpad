e99f69900bef59ea72ec2b6b401969a399a70ba2
Cheila Alves <calves.200025@gmail.com>
2022-07-27 11:02:07 +0100

    chore: remove unnecessary mocked http request

--- engine/mocks.go
+++ engine/mocks.go
@@ -6,7 +6,6 @@ package engine
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -18,7 +17,6 @@ import (
 // Use only for tests
 const defaultMockPrID = 1234
 const defaultMockPrNum = 6
-const defaultMockPrRepoName = "default-mock-repo"
 
 // Use only for tests
 var DefaultMockCtx = context.Background()
@@ -40,41 +38,10 @@ func GetDefaultMockPullRequestDetails() *github.PullRequest {
 	}
 }
 
-func getDefaultMockPullRequestFileList() *[]*github.CommitFile {
-	prRepoName := defaultMockPrRepoName
-	return &[]*github.CommitFile{
-		{
-			Filename: github.String(fmt.Sprintf("%v/file1.ts", prRepoName)),
-			Patch:    github.String(`@@ -2,9 +2,11 @@ package main
-- func previous1() {
-+ func new1() {
-+
-return`),
-		},
-		{
-			Filename: github.String(fmt.Sprintf("%v/file2.ts", prRepoName)),
-			Patch:    github.String(`@@ -2,9 +2,11 @@ package main
-- func previous2() {
-+ func new2() {
-+
-return`),
-		},
-		{
-			Filename: github.String(fmt.Sprintf("%v/file3.ts", prRepoName)),
-			Patch:    github.String(`@@ -2,9 +2,11 @@ package main
-- func previous3() {
-+ func new3() {
-+
-return`),
-		},
-	}
-}
-
 // mockDefaultHttpClient mocks an HTTP client with default values and ready for Engine.
 // Being ready for Engine means that at least the request to build Engine Env need to be mocked.
 // As for now, at least two github request need to be mocked in order to build Engine Env, mainly:
 // - Get pull request details (i.e. /repos/{owner}/{repo}/pulls/{pull_number})
-// - Get pull request files (i.e. /repos/{owner}/{repo}/pulls/{pull_number}/files)
 func mockDefaultHttpClient(clientOptions []mock.MockBackendOption) *http.Client {
 	defaultMocks := []mock.MockBackendOption{
 		mock.WithRequestMatchHandler(
@@ -82,13 +49,6 @@ func mockDefaultHttpClient(clientOptions []mock.MockBackendOption) *http.Client 
 			mock.GetReposPullsByOwnerByRepoByPullNumber,
 			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 				w.Write(mock.MustMarshal(GetDefaultMockPullRequestDetails()))
-			}),
-		),
-		mock.WithRequestMatchHandler(
-			// Mock request to get pull request changed files
-			mock.GetReposPullsFilesByOwnerByRepoByPullNumber,
-			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				w.Write(mock.MustMarshal(getDefaultMockPullRequestFileList()))
 			}),
 		),
 	}
