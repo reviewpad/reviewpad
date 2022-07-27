15252f84a44c606d38a8221f3bf0b9d0c93eb682
versoul <mailto.versoul@gmail.com>
2022-07-27 14:26:38 +0300

    feat: length utility function #159

---
+++ plugins/aladino/functions/length.go
@@ -0,0 +1,22 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions
+
+import (
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+)
+
+func Length() *aladino.BuiltInFunction {
+	return &aladino.BuiltInFunction{
+		Type: aladino.BuildFunctionType([]aladino.Type{aladino.BuildArrayOfType(aladino.BuildStringType())}, aladino.BuildIntType()),
+		Code: lengthCode,
+	}
+}
+
+func lengthCode(e aladino.Env, args []aladino.Value) (aladino.Value, error) {
+	slice := args[0].(*aladino.ArrayValue).Vals
+
+	return aladino.BuildIntValue(len(slice)), nil
+}
