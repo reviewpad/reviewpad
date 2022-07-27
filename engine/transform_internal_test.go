f2c228406a92e0c160d26c30da9a1c3849a38ae6
Cheila Alves <calves.200025@gmail.com>
2022-07-27 20:11:00 +0100

    test: add unit tests for engine transform

---
+++ engine/transform_internal_test.go
@@ -0,0 +1,55 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddDefaultTotalRequestedReviewers_WhenRegexMatchNotFound(t *testing.T) {
+    wantStr := "$addLabel(\"dummy\")"
+
+    gotStr := addDefaultTotalRequestedReviewers(wantStr)
+
+    assert.Equal(t, wantStr, gotStr)
+}
+
+func TestAddDefaultTotalRequestedReviewers_WhenRegexMatchFound(t *testing.T) {
+    str := "$assignReviewer([\"john\", \"jane\"])"
+    wantStr := "$assignReviewer([\"john\", \"jane\"], 99)"
+
+    gotStr := addDefaultTotalRequestedReviewers(str)
+
+    assert.Equal(t, wantStr, gotStr)
+}
+
+func TestAddDefaultMergeMethod_WhenGivenStringDoesNotContainMergeAction(t *testing.T) {
+    wantStr := "$addLabel(\"dummy\")"
+
+    gotStr := addDefaultMergeMethod(wantStr)
+    
+    assert.Equal(t, wantStr, gotStr)
+}
+
+
+func TestAddDefaultMergeMethod_WhenGivenStringContainsMergeAction(t *testing.T) {
+    str := "$merge()"
+    wantStr := "$merge(\"merge\")"
+
+    gotStr := addDefaultMergeMethod(str)
+    
+    assert.Equal(t, wantStr, gotStr)
+}
+
+func TestTransformActionStr(t *testing.T) {
+    str := "$assignReviewer([\"john\", \"jane\"]) $merge()"
+    wantStr := "$assignReviewer([\"john\", \"jane\"], 99) $merge(\"merge\")"
+
+    gotStr := transformActionStr(str)
+    
+    assert.Equal(t, wantStr, gotStr)
+}
