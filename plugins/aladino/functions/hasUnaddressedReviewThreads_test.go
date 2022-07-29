7bb4b299a5d1d9c5c77ca08332d344fa6f9174ee
Tiago Ferreira <ferreiratiago@outlook.com>
2022-07-29 12:21:11 +0100

    feat: Add builtin hasUnaddressedReviewThreadst

---
+++ plugins/aladino/functions/hasUnaddressedReviewThreads_test.go
@@ -0,0 +1,126 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v42/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v3/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var hasUnaddressedReviewThreads = plugins_aladino.PluginBuiltIns().Functions["hasUnaddressedReviewThreads"].Code
+
+func TestHasUnaddressedReviewThreads_WhenRequestFails(t *testing.T) {
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		nil,
+		func(w http.ResponseWriter, req *http.Request) {
+			http.Error(w, "404 Not Found", http.StatusNotFound)
+		},
+	)
+
+	_, err := hasUnaddressedReviewThreads(mockedEnv, []aladino.Value{})
+
+	assert.NotNil(t, err)
+}
+
+func TestHasUnaddressedReviewThreads(t *testing.T) {
+	mockedGraphQLQuery := fmt.Sprintf(
+		"{\"query\":\"query($pullRequestNumber:Int!$repositoryName:String!$repositoryOwner:String!$reviewThreadsCursor:String){repository(owner: $repositoryOwner, name: $repositoryName){pullRequest(number: $pullRequestNumber){reviewThreads(first: 10, after: $reviewThreadsCursor){nodes{isResolved,isOutdated},pageInfo{endCursor,hasNextPage}}}}}\",\"variables\":{\"pullRequestNumber\":%d,\"repositoryName\":\"%s\",\"repositoryOwner\":\"%s\",\"reviewThreadsCursor\":null}}\n",
+		aladino.DefaultMockPrNum,
+		aladino.DefaultMockPrRepoName,
+		aladino.DefaultMockPrOwner,
+	)
+
+	tests := map[string]struct {
+		reviewThreadsResponse string
+		wantVal               aladino.Value
+	}{
+		"when none": {
+			reviewThreadsResponse: `[]`,
+			wantVal:               aladino.BuildFalseValue(),
+		},
+		"when unresolved": {
+			reviewThreadsResponse: `[{"isResolved":false, "isOutdated":true}]`,
+			wantVal:               aladino.BuildTrueValue(),
+		},
+		"when resolved": {
+			reviewThreadsResponse: `[{"isResolved":true, "isOutdated":true}]`,
+			wantVal:               aladino.BuildFalseValue(),
+		},
+		"when outdated": {
+			reviewThreadsResponse: `[{"isResolved":true, "isOutdated":true}]`,
+			wantVal:               aladino.BuildFalseValue(),
+		},
+		"when up to date": {
+			reviewThreadsResponse: `[{"isResolved":true, "isOutdated":false}]`,
+			wantVal:               aladino.BuildTrueValue(),
+		},
+		"when more than one thread": {
+			reviewThreadsResponse: `[{"isResolved":true, "isOutdated":true}, {"isResolved":true, "isOutdated":true}]`,
+			wantVal:               aladino.BuildFalseValue(),
+		},
+	}
+
+	for name, test := range tests {
+		mockedEnv := aladino.MockDefaultEnv(
+			t,
+			[]mock.MockBackendOption{
+				mock.WithRequestMatchHandler(
+					mock.GetReposPullsByOwnerByRepoByPullNumber,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.Write(mock.MustMarshal(
+							aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+								Number: github.Int(aladino.DefaultMockPrNum),
+								Base: &github.PullRequestBranch{
+									Repo: &github.Repository{
+										Owner: &github.User{
+											Login: github.String(aladino.DefaultMockPrOwner),
+										},
+										Name: github.String(aladino.DefaultMockPrRepoName),
+									},
+								},
+							}),
+						))
+					}),
+				),
+			},
+			func(w http.ResponseWriter, req *http.Request) {
+				query := aladino.MustRead(req.Body)
+				switch query {
+				case mockedGraphQLQuery:
+					aladino.MustWrite(
+						w,
+						fmt.Sprintf(
+							`{"data": {
+                                "repository": {
+                                    "pullRequest": {
+                                        "reviewThreads": {
+                                            "nodes": %v
+                                        }
+                                    }
+                                }
+                            }}`,
+							test.reviewThreadsResponse,
+						),
+					)
+				}
+			},
+		)
+
+		t.Run(name, func(t *testing.T) {
+			gotVal, err := hasUnaddressedReviewThreads(mockedEnv, []aladino.Value{})
+
+			assert.Nil(t, err)
+			assert.Equal(t, test.wantVal, gotVal)
+		})
+	}
+}
