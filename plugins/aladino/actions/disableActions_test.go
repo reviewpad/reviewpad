0947b189789aa1591d1e8c23dba5ee5ca4364ff1
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-07-29 13:33:52 +0100

    test: failing tests

--- plugins/aladino/actions/disableActions_test.go
+++ plugins/aladino/actions/disableActions_test.go
@@ -5,6 +5,7 @@
 package plugins_aladino_actions_test
 
 import (
+	"fmt"
 	"log"
 	"testing"
 
@@ -16,13 +17,27 @@ import (
 var disableActions = plugins_aladino.PluginBuiltIns().Actions["disableActions"].Code
 
 func TestDisableActions(t *testing.T) {
-	mockedEnv, err := aladino.MockDefaultEnv(nil, nil)
+	builtInName := "emptyAction"
+
+	builtIns := &aladino.BuiltIns{
+		Actions: map[string]*aladino.BuiltInAction{
+			builtInName: {
+				Type: aladino.BuildFunctionType([]aladino.Type{aladino.BuildStringType()}, aladino.BuildArrayOfType(aladino.BuildStringType())),
+				Code: func(e aladino.Env, args []aladino.Value) error {
+					return fmt.Errorf("%v is called", builtInName)
+				},
+				Disabled: false,
+			},
+		},
+	}
+	mockedEnv, err := aladino.MockDefaultEnvWithBuiltIns(nil, nil, builtIns)
 	if err != nil {
-		log.Fatalf("mockDefaultEnv failed: %v", err)
+		log.Fatalf("mockDefaultEnvWithBuiltIns failed: %v", err)
 	}
 
-	args := []aladino.Value{aladino.BuildArrayValue([]aladino.Value{})}
+	args := []aladino.Value{aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue(builtInName)})}
 	err = disableActions(mockedEnv, args)
 
 	assert.Nil(t, err)
+	assert.True(t, mockedEnv.GetBuiltIns().Actions[builtInName].Disabled)
 }
