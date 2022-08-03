2d0c03854cbda8eee17420702cc8a00184a3fda5
Zelalem Mekonen <zola@programmer.net>
2022-08-03 18:13:53 +0300

    $addToProject built in action (#203)
    * feat: added addToProject builtin action
    
    * test: added test coverage for addToProject action
    
    * test: fix addToProject tests after rebasing
    
    * feat: add project utilities
    
    * feat: handle fields pagination
    
    * refactor: remove unnecessary empty lines
    
    * test: refactor tests for more readability
    
    * refactor: simplify get project fields function
    
    * test: update add to project tests
    
    * feat: added addToProject builtin action
    
    * docs: added warning on graphql query used on get project by name
    
    * refactor: removed t.Log statment from test

---
+++ utils/github_projects.go
@@ -0,0 +1,114 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file
+package utils
+
+import (
+	"context"
+	"errors"
+
+	"github.com/shurcooL/githubv4"
+)
+
+var (
+	ErrProjectNotFound = errors.New("project not found")
+)
+
+type ProjectV2 struct {
+	ID     string
+	Number uint64
+}
+
+type PageInfo struct {
+	HasNextPage bool
+	EndCursor   string
+}
+
+type Fields struct {
+	PageInfo PageInfo
+	Nodes    []FieldNode
+}
+
+type FieldNode struct {
+	Details FieldDetails `graphql:"... on ProjectV2SingleSelectField"`
+}
+
+type FieldDetails struct {
+	ID      string
+	Name    string
+	Options []struct {
+		ID   string
+		Name string
+	}
+}
+
+func GetProjectV2ByName(ctx context.Context, client *githubv4.Client, owner, repo, name string) (*ProjectV2, error) {
+	// Warning: we've faced trouble before with the Github GraphQL API, we'll update this later when we find a better alternative.
+	var getProjectV2ByNameQuery struct {
+		Repository struct {
+			ProjectsV2 struct {
+				Nodes []ProjectV2
+			} `graphql:"projectsV2(query: $name, first: 1, orderBy: {field: TITLE, direction: ASC})"`
+		} `graphql:"repository(owner: $repositoryOwner, name: $repositoryName)"`
+	}
+
+	varGetProjectV2ByNameQueryVariables := map[string]interface{}{
+		"repositoryOwner": githubv4.String(owner),
+		"repositoryName":  githubv4.String(repo),
+		"name":            githubv4.String(name),
+	}
+
+	if err := client.Query(ctx, &getProjectV2ByNameQuery, varGetProjectV2ByNameQueryVariables); err != nil {
+		return nil, err
+	}
+
+	if len(getProjectV2ByNameQuery.Repository.ProjectsV2.Nodes) == 0 {
+		return nil, ErrProjectNotFound
+	}
+
+	return &getProjectV2ByNameQuery.Repository.ProjectsV2.Nodes[0], nil
+}
+
+func GetProjectFieldsByProjectNumber(ctx context.Context, client *githubv4.Client, owner, repo string, projectNumber uint64, retryCount int) ([]FieldNode, error) {
+	fields := []FieldNode{}
+	hasNextPage := true
+	currentRequestRetry := 1
+
+	var getProjectFieldsQuery struct {
+		Repository struct {
+			ProjectV2 *struct {
+				Fields Fields `graphql:"fields(first: 50, after: $afterCursor, orderBy: {field: NAME, direction: ASC})"`
+			} `graphql:"projectV2(number: $projectNumber)"`
+		} `graphql:"repository(owner: $repositoryOwner, name: $repositoryName)"`
+	}
+
+	varGQLGetProjectFieldsQuery := map[string]interface{}{
+		"repositoryOwner": githubv4.String(owner),
+		"repositoryName":  githubv4.String(repo),
+		"projectNumber":   githubv4.Int(projectNumber),
+		"afterCursor":     githubv4.String(""),
+	}
+
+	for hasNextPage {
+		if err := client.Query(ctx, &getProjectFieldsQuery, varGQLGetProjectFieldsQuery); err != nil {
+			currentRequestRetry++
+			if currentRequestRetry <= retryCount {
+				continue
+			}
+			return nil, err
+		}
+
+		project := getProjectFieldsQuery.Repository.ProjectV2
+		if project == nil {
+			return nil, ErrProjectNotFound
+		}
+
+		fields = append(fields, project.Fields.Nodes...)
+
+		hasNextPage = project.Fields.PageInfo.HasNextPage
+
+		varGQLGetProjectFieldsQuery["afterCursor"] = githubv4.String(project.Fields.PageInfo.EndCursor)
+	}
+
+	return fields, nil
+}
---
+++ utils/github_projects_test.go
@@ -0,0 +1,312 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file
+package utils_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	plugins_aladino_actions "github.com/reviewpad/reviewpad/v3/plugins/aladino/actions"
+	"github.com/reviewpad/reviewpad/v3/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetProjectV2ByName_WhenRequestFails(t *testing.T) {
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		nil,
+		func(w http.ResponseWriter, req *http.Request) {
+			http.Error(w, "404 Not Found", http.StatusNotFound)
+		},
+	)
+	mockOwner := utils.GetPullRequestBaseOwnerName(mockedEnv.GetPullRequest())
+	mockRepo := utils.GetPullRequestBaseRepoName(mockedEnv.GetPullRequest())
+	mockProjectName := "reviewpad"
+
+	project, err := utils.GetProjectV2ByName(mockedEnv.GetCtx(), mockedEnv.GetClientGQL(), mockOwner, mockRepo, mockProjectName)
+
+	assert.NotNil(t, err)
+
+	assert.Nil(t, project)
+}
+
+func TestGetProjectV2ByName_WhenProjectNotFound(t *testing.T) {
+	mockedGetProjectByNameQuery := `{
+        "query": "query($name:String! $repositoryName:String! $repositoryOwner:String!) {
+            repository(owner: $repositoryOwner, name: $repositoryName) {
+                projectsV2(query: $name, first: 1, orderBy: {field: TITLE, direction: ASC}) {
+                    nodes{
+                        id,
+                        number
+                    }
+                }
+            }
+        }",
+        "variables": {
+            "name": "reviewpad",
+            "repositoryName": "default-mock-repo",
+            "repositoryOwner": "john"
+        }
+    }`
+	mockedGetProjectByNameQueryBody := `{
+        "data": {
+            "repository": {
+                "projectsV2":{
+                    "nodes":[]
+                }
+            }
+        }
+    }`
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		[]mock.MockBackendOption{},
+		func(res http.ResponseWriter, req *http.Request) {
+			query := utils.MinifyQuery(aladino.MustRead(req.Body))
+			switch query {
+			case utils.MinifyQuery(mockedGetProjectByNameQuery):
+				aladino.MustWrite(
+					res,
+					mockedGetProjectByNameQueryBody,
+				)
+			}
+		},
+	)
+	mockOwner := utils.GetPullRequestBaseOwnerName(mockedEnv.GetPullRequest())
+	mockRepo := utils.GetPullRequestBaseRepoName(mockedEnv.GetPullRequest())
+	mockProjectName := "reviewpad"
+
+	project, err := utils.GetProjectV2ByName(mockedEnv.GetCtx(), mockedEnv.GetClientGQL(), mockOwner, mockRepo, mockProjectName)
+
+	assert.Equal(t, plugins_aladino_actions.ErrProjectNotFound, err)
+
+	assert.Nil(t, project)
+}
+
+func TestGetProjectV2ByName_WhenProjectFound(t *testing.T) {
+	mockedGetProjectByNameQuery := `{
+        "query": "query($name:String! $repositoryName:String! $repositoryOwner:String!) {
+            repository(owner: $repositoryOwner, name: $repositoryName) {
+                projectsV2(query: $name, first: 1, orderBy: {field: TITLE, direction: ASC}) {
+                    nodes{id,number}
+                }
+            }
+        }",
+        "variables": {
+            "name":"reviewpad",
+            "repositoryName":"default-mock-repo",
+            "repositoryOwner":"john"
+        }
+    }`
+	mockedGetProjectByNameQueryBody := `{
+        "data": {
+            "repository":{
+                "projectsV2":{
+                    "nodes":[
+                        {
+                            "id": "1",
+                            "number": 1
+                        }
+                    ]
+                }
+            }
+        }
+    }`
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		[]mock.MockBackendOption{},
+		func(res http.ResponseWriter, req *http.Request) {
+			query := utils.MinifyQuery(aladino.MustRead(req.Body))
+			switch query {
+			case utils.MinifyQuery(mockedGetProjectByNameQuery):
+				aladino.MustWrite(
+					res,
+					mockedGetProjectByNameQueryBody,
+				)
+			}
+		},
+	)
+	mockOwner := utils.GetPullRequestBaseOwnerName(mockedEnv.GetPullRequest())
+	mockRepo := utils.GetPullRequestBaseRepoName(mockedEnv.GetPullRequest())
+	mockProjectName := "reviewpad"
+
+	project, err := utils.GetProjectV2ByName(mockedEnv.GetCtx(), mockedEnv.GetClientGQL(), mockOwner, mockRepo, mockProjectName)
+
+	assert.Equal(t, nil, err)
+
+	assert.NotNil(t, project)
+}
+
+func TestGetProjectFieldsByProjectNumber_WhenRequestFails(t *testing.T) {
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		nil,
+		func(w http.ResponseWriter, req *http.Request) {
+			http.Error(w, "404 Not Found", http.StatusNotFound)
+		},
+	)
+	mockOwner := utils.GetPullRequestBaseOwnerName(mockedEnv.GetPullRequest())
+	mockRepo := utils.GetPullRequestBaseRepoName(mockedEnv.GetPullRequest())
+	mockProjectNumber := 1
+	mockRetryCount := 1
+
+	project, err := utils.GetProjectFieldsByProjectNumber(mockedEnv.GetCtx(), mockedEnv.GetClientGQL(), mockOwner, mockRepo, uint64(mockProjectNumber), mockRetryCount)
+
+	assert.NotNil(t, err)
+
+	assert.Nil(t, project)
+}
+
+func TestGetProjectFieldsByProjectNumber_WhenProjectNotFound(t *testing.T) {
+	mockedGetProjectByNameQuery := `{
+        "query": "query($afterCursor:String! $projectNumber:Int! $repositoryName:String! $repositoryOwner:String!) {
+            repository(owner: $repositoryOwner, name: $repositoryName) {
+                projectV2(number: $projectNumber) {
+                    fields(first: 50, after: $afterCursor, orderBy: {field: NAME, direction: ASC}) {
+                        pageInfo {
+                            hasNextPage,
+                            endCursor
+                        },
+                        nodes{
+                            ... on ProjectV2SingleSelectField {
+                                id,
+                                name,
+                                options {
+                                    id,
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }",
+        "variables": {
+            "afterCursor": "",
+            "projectNumber": 1,
+            "repositoryName": "default-mock-repo",
+            "repositoryOwner":"john"
+        }
+    }`
+	mockedGetProjectByNameQueryBody := `{
+        "data": {
+            "repository":{
+                "projectV2": null
+            }
+        }
+    }`
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		[]mock.MockBackendOption{},
+		func(res http.ResponseWriter, req *http.Request) {
+			query := utils.MinifyQuery(aladino.MustRead(req.Body))
+			switch query {
+			case utils.MinifyQuery(mockedGetProjectByNameQuery):
+				aladino.MustWrite(
+					res,
+					mockedGetProjectByNameQueryBody,
+				)
+			}
+		},
+	)
+	mockOwner := utils.GetPullRequestBaseOwnerName(mockedEnv.GetPullRequest())
+	mockRepo := utils.GetPullRequestBaseRepoName(mockedEnv.GetPullRequest())
+	mockProjectNumber := 1
+	mockRetryCount := 1
+
+	project, err := utils.GetProjectFieldsByProjectNumber(mockedEnv.GetCtx(), mockedEnv.GetClientGQL(), mockOwner, mockRepo, uint64(mockProjectNumber), mockRetryCount)
+
+	assert.Equal(t, plugins_aladino_actions.ErrProjectNotFound, err)
+
+	assert.Nil(t, project)
+
+}
+
+func TestGetProjectFieldsByProjectNumber_WhenRetrySuccessful(t *testing.T) {
+	mockedGetProjectByNameQuery := `{
+        "query":"query($afterCursor:String! $projectNumber:Int! $repositoryName:String! $repositoryOwner:String!) {
+            repository(owner: $repositoryOwner, name: $repositoryName) {
+                projectV2(number: $projectNumber) {
+                    fields(first: 50, after: $afterCursor, orderBy: {field: NAME, direction: ASC}) {
+                        pageInfo{
+                            hasNextPage,
+                            endCursor
+                        },
+                        nodes{
+                            ... on ProjectV2SingleSelectField {
+                                id,
+                                name,
+                                options {
+                                    id,
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }",
+        "variables":{
+            "afterCursor":"",
+            "projectNumber":1,
+            "repositoryName":
+            "default-mock-repo",
+            "repositoryOwner":"john"
+        }
+    }`
+	mockedGetProjectFieldsQueryBody := `{
+        "data": {
+            "repository":{
+                "projectV2": {
+                    "fields": {
+                        "pageInfo": {
+                            "hasNextPage": false,
+                            "endCursor": ""
+                        },
+                        "nodes": [
+                            {
+                                "id": "1",
+                                "name": "status",
+                                "options": []
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }`
+	currentTry := 1
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		[]mock.MockBackendOption{},
+		func(res http.ResponseWriter, req *http.Request) {
+			query := utils.MinifyQuery(aladino.MustRead(req.Body))
+			switch query {
+			case utils.MinifyQuery(mockedGetProjectByNameQuery):
+				if currentTry == 2 {
+					aladino.MustWrite(
+						res,
+						mockedGetProjectFieldsQueryBody,
+					)
+					return
+				}
+				currentTry++
+				aladino.MustWrite(res, "")
+			}
+		},
+	)
+	mockOwner := utils.GetPullRequestBaseOwnerName(mockedEnv.GetPullRequest())
+	mockRepo := utils.GetPullRequestBaseRepoName(mockedEnv.GetPullRequest())
+	mockProjectNumber := 1
+	mockRetryCount := 2
+
+	fields, err := utils.GetProjectFieldsByProjectNumber(mockedEnv.GetCtx(), mockedEnv.GetClientGQL(), mockOwner, mockRepo, uint64(mockProjectNumber), mockRetryCount)
+
+	assert.Equal(t, nil, err)
+
+	assert.NotNil(t, fields)
+
+	assert.Equal(t, 1, len(fields))
+}
