839caf6031b0dab4c4e33d4c803cb0f5a2fd2ebb
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-08-11 10:53:46 +0100

    feat: add download contents to codehost

--- codehost/github/repositories.go
+++ codehost/github/repositories.go
@@ -6,10 +6,27 @@ package github
 
 import (
 	"context"
+	"io/ioutil"
 
 	"github.com/google/go-github/v45/github"
 )
 
 func (c *GithubClient) GetRepositoryBranch(ctx context.Context, owner string, repo string, branch string, followRedirects bool) (*github.Branch, *github.Response, error) {
 	return c.clientREST.Repositories.GetBranch(ctx, owner, repo, branch, followRedirects)
+}
+
+func (c *GithubClient) DownloadContents(ctx context.Context, filePath string, branch *github.PullRequestBranch) ([]byte, error) {
+	branchRepoOwner := *branch.Repo.Owner.Login
+	branchRepoName := *branch.Repo.Name
+	branchRef := *branch.Ref
+
+	ioReader, _, err := c.clientREST.Repositories.DownloadContents(ctx, branchRepoOwner, branchRepoName, filePath, &github.RepositoryContentGetOptions{
+		Ref: branchRef,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(ioReader)
 }
