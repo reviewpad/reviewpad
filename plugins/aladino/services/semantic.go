d6ad69e8c547d1cd677a8455d12141eb2599cbfd
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-08-11 21:18:32 +0100

    feat: add services package

---
+++ plugins/aladino/services/semantic.go
@@ -0,0 +1,16 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_services
+
+import (
+	"github.com/reviewpad/api/go/clients"
+	"github.com/reviewpad/api/go/services"
+)
+
+func NewSemanticService(endpoint string) services.SemanticClient {
+	client, _ := clients.NewSemanticClient("http://localhost:8080")
+
+	return client
+}
