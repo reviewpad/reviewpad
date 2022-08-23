199cea1db91e678dcb5013c4359de38a2da0a905
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-08-23 13:02:34 +0100

    test: update env test

--- engine/env_test.go
+++ engine/env_test.go
@@ -7,7 +7,6 @@ package engine_test
 import (
 	"testing"
 
-	"github.com/google/go-github/v45/github"
 	"github.com/reviewpad/reviewpad/v3/engine"
 	"github.com/reviewpad/reviewpad/v3/lang/aladino"
 	"github.com/stretchr/testify/assert"
@@ -17,32 +16,12 @@ func TestNewEvalEnv(t *testing.T) {
 	ctx := engine.DefaultMockCtx
 	githubClient := engine.MockGithubClient(nil)
 	collector := engine.DefaultMockCollector
-	mockedPullRequest := engine.GetDefaultMockPullRequestDetails()
-	fileName := "default-mock-repo/file1.ts"
-	patch := `@@ -2,9 +2,11 @@ package main
-- func previous1() {
-+ func new1() {
-+
-return`
-
-	mockedFile := &aladino.File{
-		Repr: &github.CommitFile{
-			Filename: github.String(fileName),
-			Patch:    github.String(patch),
-		},
-	}
-	mockedFile.AppendToDiff(false, 2, 2, 2, 3, " func previous1() {", " func new1() {\n")
-	mockedPatch := aladino.Patch{
-		fileName: mockedFile,
-	}
 
 	mockedAladinoInterpreter := &aladino.Interpreter{
 		Env: &aladino.BaseEnv{
 			Ctx:          ctx,
 			GithubClient: githubClient,
 			Collector:    collector,
-			PullRequest:  mockedPullRequest,
-			Patch:        mockedPatch,
 			RegisterMap:  aladino.RegisterMap(make(map[string]aladino.Value)),
 			BuiltIns:     aladino.MockBuiltIns(),
 			Report:       &aladino.Report{Actions: make([]string, 0)},
