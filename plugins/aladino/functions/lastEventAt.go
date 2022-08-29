0ee351a1f3213218529f49821c29526a85ab59b4
Cheila Alves <calves.200025@gmail.com>
2022-08-29 09:34:15 +0100

    fix: handler import in lastEventAt built-in

--- plugins/aladino/functions/lastEventAt.go
+++ plugins/aladino/functions/lastEventAt.go
@@ -7,7 +7,7 @@ package plugins_aladino_functions
 import (
 	"time"
 
-	"github.com/reviewpad/host-event-handler/handler"
+	"github.com/reviewpad/reviewpad/v3/handler"
 	"github.com/reviewpad/reviewpad/v3/lang/aladino"
 )
 
