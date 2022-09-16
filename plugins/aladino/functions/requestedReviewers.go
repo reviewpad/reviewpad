3dfd98a702699a6e55c89b20025a5d122de4e249
Cheila Alves <calves.200025@gmail.com>
2022-09-16 16:53:19 +0100

    feat: $requestedReviewers built-in

---
+++ plugins/aladino/functions/requestedReviewers.go
@@ -0,0 +1,37 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions
+
+import (
+	"github.com/reviewpad/reviewpad/v3/codehost/github/target"
+	"github.com/reviewpad/reviewpad/v3/handler"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+)
+
+func RequestedReviewers() *aladino.BuiltInFunction {
+	return &aladino.BuiltInFunction{
+		Type:           aladino.BuildFunctionType([]aladino.Type{}, aladino.BuildArrayOfType(aladino.BuildStringType())),
+		Code:           requestedReviewersCode,
+		SupportedKinds: []handler.TargetEntityKind{handler.PullRequest},
+	}
+}
+
+func requestedReviewersCode(e aladino.Env, _ []aladino.Value) (aladino.Value, error) {
+	pullRequest := e.GetTarget().(*target.PullRequestTarget).PullRequest
+	usersReviewers := pullRequest.RequestedReviewers
+	teamReviewers := pullRequest.RequestedTeams
+	totalReviewers := len(usersReviewers) + len(teamReviewers)
+	reviewersLogin := make([]aladino.Value, totalReviewers)
+
+	for i, userReviewer := range usersReviewers {
+		reviewersLogin[i] = aladino.BuildStringValue(userReviewer.GetLogin())
+	}
+
+	for i, teamReviewer := range teamReviewers {
+		reviewersLogin[i+len(usersReviewers)] = aladino.BuildStringValue(teamReviewer.GetSlug())
+	}
+
+	return aladino.BuildArrayValue(reviewersLogin), nil
+}
---
+++ plugins/aladino/functions/requestedReviewers_test.go
@@ -0,0 +1,59 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v45/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v3/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var requestedReviewers = plugins_aladino.PluginBuiltIns().Functions["requestedReviewers"].Code
+
+func TestRequestedReviewers(t *testing.T) {
+	ghUsersReviewers := []*github.User{
+		{Login: github.String("mary")},
+	}
+	ghTeamReviewers := []*github.Team{
+		{Slug: github.String("reviewpad")},
+	}
+	mockedPullRequest := aladino.GetDefaultMockPullRequestDetailsWith(&github.PullRequest{
+		User: &github.User{
+			Login: github.String("john"),
+		},
+		RequestedReviewers: ghUsersReviewers,
+		RequestedTeams:     ghTeamReviewers,
+	})
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		[]mock.MockBackendOption{
+			mock.WithRequestMatchHandler(
+				mock.GetReposPullsByOwnerByRepoByPullNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Write(mock.MustMarshal(mockedPullRequest))
+				}),
+			),
+		},
+		nil,
+		aladino.MockBuiltIns(),
+		nil,
+	)
+
+	wantReviewers := aladino.BuildArrayValue([]aladino.Value{
+		aladino.BuildStringValue("mary"),
+		aladino.BuildStringValue("reviewpad"),
+	})
+
+	args := []aladino.Value{}
+	gotReviewers, err := requestedReviewers(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantReviewers, gotReviewers)
+}
