fd8f3ab235eae773bbf6a049cd4412ea3bf7f1e2
Cheila Alves <calves.200025@gmail.com>
2022-10-20 16:59:59 +0100

    chore: add log

--- codehost/github/client.go
+++ codehost/github/client.go
@@ -6,6 +6,7 @@ package github
 
 import (
 	"context"
+	"log"
 
 	"github.com/google/go-github/v45/github"
 	"github.com/shurcooL/githubv4"
@@ -31,6 +32,7 @@ func NewGithubClientFromToken(ctx context.Context, token string) *GithubClient {
 	tc := oauth2.NewClient(ctx, ts)
 
 	clientREST := github.NewClient(tc)
+	log.Printf("ctx: %+v", clientREST.BaseURL.User)
 	clientGQL := githubv4.NewClient(tc)
 
 	return &GithubClient{
