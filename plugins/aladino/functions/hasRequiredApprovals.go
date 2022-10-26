51183f094278860d9d832c8893fafe5fd2a51d16
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2022-10-26 15:17:13 +0100

    feat: hasRequiredApprovals built-in (#412)

---
+++ plugins/aladino/functions/hasRequiredApprovals.go
@@ -0,0 +1,75 @@
+package plugins_aladino_functions
+
+import (
+	"fmt"
+
+	"github.com/reviewpad/reviewpad/v3/codehost/github/target"
+	"github.com/reviewpad/reviewpad/v3/handler"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+)
+
+func HasRequiredApprovals() *aladino.BuiltInFunction {
+	return &aladino.BuiltInFunction{
+		Type: aladino.BuildFunctionType(
+			[]aladino.Type{
+				aladino.BuildIntType(),
+				aladino.BuildArrayOfType(aladino.BuildStringType()),
+			},
+			aladino.BuildBoolType(),
+		),
+		Code:           hasRequiredApprovalsCode,
+		SupportedKinds: []handler.TargetEntityKind{handler.PullRequest},
+	}
+}
+
+func hasRequiredApprovalsCode(e aladino.Env, args []aladino.Value) (aladino.Value, error) {
+	pullRequest := e.GetTarget().(*target.PullRequestTarget)
+	totalRequiredApprovals := args[0].(*aladino.IntValue).Val
+	requiredApprovalsFrom := args[1].(*aladino.ArrayValue).Vals
+
+	if len(requiredApprovalsFrom) < totalRequiredApprovals {
+		return nil, fmt.Errorf("hasRequiredApprovals: the number of required approvals exceeds the number of members from the given list of required approvals")
+	}
+
+	approvedBy, err := getUserLoginsFromApprovals(pullRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	totalApprovedReviews := 0
+	for _, requiredApproval := range requiredApprovalsFrom {
+		if contains(approvedBy, requiredApproval.(*aladino.StringValue).Val) {
+			totalApprovedReviews++
+		}
+	}
+
+	return aladino.BuildBoolValue(totalApprovedReviews >= totalRequiredApprovals), nil
+}
+
+func getUserLoginsFromApprovals(pullRequest *target.PullRequestTarget) ([]string, error) {
+	reviews, err := pullRequest.GetReviews()
+	if err != nil {
+		return nil, err
+	}
+
+	approvedBy := make([]string, 0)
+	for _, review := range reviews {
+		user := review.User.Login
+
+		if review.State == "APPROVED" && !contains(approvedBy, user) {
+			approvedBy = append(approvedBy, user)
+		}
+	}
+
+	return approvedBy, nil
+}
+
+func contains(slice []string, str string) bool {
+	for _, elem := range slice {
+		if elem == str {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ plugins/aladino/functions/hasRequiredApprovals_test.go
@@ -0,0 +1,122 @@
+package plugins_aladino_functions_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v45/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v3/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var hasRequiredApprovals = plugins_aladino.PluginBuiltIns().Functions["hasRequiredApprovals"].Code
+
+func TestHasRequiredApprovals_WhenErrorOccurs(t *testing.T) {
+	failMessage := "ListReviewsRequestFail"
+
+	tests := map[string]struct {
+		clientOptions             []mock.MockBackendOption
+		inputTotalRequiredReviews aladino.Value
+		inputRequiredReviewsFrom  aladino.Value
+		wantErr                   string
+	}{
+		"when given total required approvals exceeds the size of the given list of required approvals": {
+			clientOptions:             []mock.MockBackendOption{},
+			inputTotalRequiredReviews: aladino.BuildIntValue(2),
+			inputRequiredReviewsFrom:  aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue("john")}),
+			wantErr:                   "hasRequiredApprovals: the number of required approvals exceeds the number of members from the given list of required approvals",
+		},
+		"when list reviews request fails": {
+			clientOptions: []mock.MockBackendOption{
+				mock.WithRequestMatchHandler(
+					mock.GetReposPullsReviewsByOwnerByRepoByPullNumber,
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						mock.WriteError(
+							w,
+							http.StatusInternalServerError,
+							failMessage,
+						)
+					}),
+				),
+			},
+			inputTotalRequiredReviews: aladino.BuildIntValue(1),
+			inputRequiredReviewsFrom:  aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue("john"), aladino.BuildStringValue("mary")}),
+			wantErr:                   failMessage,
+		},
+	}
+
+	for _, test := range tests {
+		mockedEnv := aladino.MockDefaultEnv(t, test.clientOptions, nil, aladino.MockBuiltIns(), nil)
+
+		args := []aladino.Value{test.inputTotalRequiredReviews, test.inputRequiredReviewsFrom}
+		gotHasRequiredApprovals, gotErr := hasRequiredApprovals(mockedEnv, args)
+
+		assert.Nil(t, gotHasRequiredApprovals)
+		assert.NotNil(t, gotErr)
+
+		var gotErrMessage string
+		if err, ok := gotErr.(*github.ErrorResponse); ok {
+			gotErrMessage = err.Message
+		} else {
+			gotErrMessage = gotErr.Error()
+		}
+
+		assert.Equal(t, test.wantErr, gotErrMessage)
+	}
+}
+
+func TestHasRequiredApprovals(t *testing.T) {
+	tests := map[string]struct {
+		clientOptions             []mock.MockBackendOption
+		inputTotalRequiredReviews aladino.Value
+		inputRequiredReviewsFrom  aladino.Value
+		wantHasRequiredApprovals  aladino.Value
+		wantErr                   string
+	}{
+		"when there is not enough required approvals": {
+			clientOptions: []mock.MockBackendOption{
+				mock.WithRequestMatch(
+					mock.GetReposPullsReviewsByOwnerByRepoByPullNumber,
+					[]*github.PullRequestReview{},
+				),
+			},
+			inputTotalRequiredReviews: aladino.BuildIntValue(1),
+			inputRequiredReviewsFrom:  aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue("john"), aladino.BuildStringValue("mary")}),
+			wantHasRequiredApprovals:  aladino.BuildBoolValue(false),
+			wantErr:                   "",
+		},
+		"when there is enough required approvals": {
+			clientOptions: []mock.MockBackendOption{
+				mock.WithRequestMatch(
+					mock.GetReposPullsReviewsByOwnerByRepoByPullNumber,
+					[]*github.PullRequestReview{
+						{
+							ID:    github.Int64(1),
+							Body:  github.String("Here is the body for the review."),
+							State: github.String("APPROVED"),
+							User: &github.User{
+								Login: github.String("mary"),
+							},
+						},
+					},
+				),
+			},
+			inputTotalRequiredReviews: aladino.BuildIntValue(1),
+			inputRequiredReviewsFrom:  aladino.BuildArrayValue([]aladino.Value{aladino.BuildStringValue("john"), aladino.BuildStringValue("mary")}),
+			wantHasRequiredApprovals:  aladino.BuildBoolValue(true),
+			wantErr:                   "",
+		},
+	}
+
+	for _, test := range tests {
+		mockedEnv := aladino.MockDefaultEnv(t, test.clientOptions, nil, aladino.MockBuiltIns(), nil)
+
+		args := []aladino.Value{test.inputTotalRequiredReviews, test.inputRequiredReviewsFrom}
+		gotHasRequiredApprovals, gotErr := hasRequiredApprovals(mockedEnv, args)
+
+		assert.Nil(t, gotErr)
+		assert.Equal(t, test.wantHasRequiredApprovals, gotHasRequiredApprovals)
+	}
+}
