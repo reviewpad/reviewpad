b3b94e818793001303772359e02a9349d9f2902e
Zelalem Mekonen <zola@programmer.net>
2022-11-03 14:35:47 +0300

    test: add more transform test cases

--- engine/transform_internal_test.go
+++ engine/transform_internal_test.go
@@ -63,9 +63,21 @@ func TestTransformAladinoExpression(t *testing.T) {
 			arg:     `$close("comment")`,
 			wantVal: `$close("comment", "completed")`,
 		},
-		"close with comment and state reason": {
+		"close with comment and not_planned state reason": {
 			arg:     `$close("comment", "not_planned")`,
 			wantVal: `$close("comment", "not_planned")`,
+		},
+		"close with comment and completed state reason": {
+			arg:     `$close("comment", "completed")`,
+			wantVal: `$close("comment", "completed")`,
+		},
+		"close with empty comment and completed state reason": {
+			arg:     `$close("", "completed")`,
+			wantVal: `$close("", "completed")`,
+		},
+		"close with empty comment and not_planned state reason": {
+			arg:     `$close("", "not_planned")`,
+			wantVal: `$close("", "not_planned")`,
 		},
 		// TODO: test addDefaultTotalRequestedReviewers
 	}
