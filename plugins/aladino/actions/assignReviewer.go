140e50965e18f93ab9202d8c13c5c400142695ff
Zelalem Mekonen <zola@programmer.net>
2022-11-11 17:40:10 +0300

    fix: check last review time is before last push time before re requesting review

--- plugins/aladino/actions/assignReviewer.go
+++ plugins/aladino/actions/assignReviewer.go
@@ -37,6 +37,11 @@ func assignReviewerCode(e aladino.Env, args []aladino.Value) error {
 	totalRequiredReviewers := args[1].(*aladino.IntValue).Val
 	policy := args[2].(*aladino.StringValue).Val
 	target := e.GetTarget().(*target.PullRequestTarget)
+	targetEntity := target.GetTargetEntity()
+	ctx := e.GetCtx()
+	owner := targetEntity.Owner
+	repo := targetEntity.Repo
+	prNum := targetEntity.Number
 
 	allowedPolicies := map[string]bool{"random": true, "round-robin": true, "reviewpad": true}
 	if _, ok := allowedPolicies[policy]; !ok {
@@ -71,12 +76,17 @@ func assignReviewerCode(e aladino.Env, args []aladino.Value) error {
 		return err
 	}
 
+	lastPushDate, err := e.GetGithubClient().GetPullRequestLastPushDate(ctx, owner, repo, prNum)
+	if err != nil {
+		return err
+	}
+
 	// Re-request current reviewers only when last review status is not APPROVED
 	for _, availableReviewer := range availableReviewers {
 		userLogin := availableReviewer.(*aladino.StringValue).Val
 		if codehost.HasReview(reviews, userLogin) {
 			lastReview := codehost.LastReview(reviews, userLogin)
-			if lastReview.State != "APPROVED" {
+			if lastReview.State != "APPROVED" && lastReview.SubmittedAt.Before(lastPushDate) {
 				reviewers = append(reviewers, userLogin)
 			} else {
 				log.Printf("assignReviewer: reviewer %v has already approved the pull request", userLogin)
