46c71a216ba10e39607211fae1e9f8289ab41f61
Zelalem Mekonen <zola@programmer.net>
2022-11-16 15:24:13 +0300

    fix: ignore extra args to sprintf built-in action

--- plugins/aladino/functions/sprintf.go
+++ plugins/aladino/functions/sprintf.go
@@ -6,9 +6,14 @@ package plugins_aladino_functions
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/reviewpad/reviewpad/v3/handler"
 	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+)
+
+var (
+	verbsRegExp = regexp.MustCompile(`%(\#|\+|\-| |0)?(\[\d+\])?(([1-9])\.([1-9])|([1-9])|([1-9])\.|\.([1-9]))?(\w{1,9})`)
 )
 
 func Sprintf() *aladino.BuiltInFunction {
@@ -23,6 +28,12 @@ func sprintfCode(e aladino.Env, args []aladino.Value) (aladino.Value, error) {
 	var clearVals []interface{}
 	format := args[0].(*aladino.StringValue).Val
 	vals := args[1].(*aladino.ArrayValue).Vals
+
+	verbs := verbsRegExp.FindAllString(format, -1)
+
+	if len(vals) > len(verbs) {
+		vals = vals[0:len(verbs)]
+	}
 
 	for _, val := range vals {
 		switch val.(type) {
