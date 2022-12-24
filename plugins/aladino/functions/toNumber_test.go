c763b1bca3ed872e3357f6f61a6afe89392b455f
Zelalem Mekonen <zola@programmer.net>
2022-12-24 14:37:50 +0300

    test: add coverage for toNumber built-in function

---
+++ plugins/aladino/functions/toNumber_test.go
@@ -0,0 +1,66 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package plugins_aladino_functions_test
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v3/plugins/aladino"
+	"github.com/stretchr/testify/assert"
+)
+
+var toNumber = plugins_aladino.PluginBuiltIns().Functions["toNumber"].Code
+
+func TestToNumber(t *testing.T) {
+	tests := map[string]struct {
+		str     string
+		wantRes aladino.Value
+		wantErr error
+	}{
+		"when empty": {
+			str: "",
+			wantErr: fmt.Errorf(`error converting "" to number: %w`, &strconv.NumError{
+				Func: "Atoi",
+				Num:  "",
+				Err:  errors.New("invalid syntax"),
+			}),
+		},
+		"when not a number": {
+			str: "a",
+			wantErr: fmt.Errorf(`error converting "a" to number: %w`, &strconv.NumError{
+				Func: "Atoi",
+				Num:  "a",
+				Err:  errors.New("invalid syntax"),
+			}),
+		},
+		"when zero": {
+			str:     "0",
+			wantRes: aladino.BuildIntValue(0),
+		},
+		"when negative": {
+			str:     "-1",
+			wantRes: aladino.BuildIntValue(-1),
+		},
+		"when positive": {
+			str:     "1",
+			wantRes: aladino.BuildIntValue(1),
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			env := aladino.MockDefaultEnv(t, nil, nil, aladino.MockBuiltIns(), nil)
+
+			res, err := toNumber(env, []aladino.Value{aladino.BuildStringValue(test.str)})
+
+			assert.Equal(t, test.wantRes, res)
+			assert.Equal(t, test.wantErr, err)
+		})
+	}
+}
