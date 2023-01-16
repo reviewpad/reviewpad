99d4f74e8a2b03e454cc5d6f24d7434271d00c97
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2023-01-16 16:28:14 +0000

    feat: embed cookbook package

---
+++ cookbook/recipes.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2022 Explore.dev, Unipessoal Lda - All Rights Reserved
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package cookbook
+
+import (
+	"fmt"
+
+	"github.com/reviewpad/reviewpad/v3/codehost/github"
+	"github.com/reviewpad/reviewpad/v3/cookbook/recipes"
+	"github.com/sirupsen/logrus"
+)
+
+type Cookbook interface {
+	GetRecipe(name string) (recipes.Recipe, error)
+}
+
+type cookbook struct {
+	gitHubClient *github.GithubClient
+	logger       *logrus.Entry
+}
+
+func NewCookbook(logger *logrus.Entry, gitHubClient *github.GithubClient) Cookbook {
+	return &cookbook{
+		gitHubClient: gitHubClient,
+		logger:       logger,
+	}
+}
+
+func (c *cookbook) GetRecipe(name string) (recipes.Recipe, error) {
+	switch name {
+	case "size":
+		return recipes.NewSizeRecipe(c.logger.WithField("recipe", "size"), c.gitHubClient), nil
+	default:
+		return nil, fmt.Errorf("recipe %s not found", name)
+	}
+}
---
+++ cookbook/recipes/recipe.go
@@ -0,0 +1,15 @@
+// Copyright (C) 2022 Explore.dev, Unipessoal Lda - All Rights Reserved
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package recipes
+
+import (
+	"context"
+
+	"github.com/reviewpad/reviewpad/v3/handler"
+)
+
+type Recipe interface {
+	Run(context context.Context, targetEntity handler.TargetEntity) error
+}
