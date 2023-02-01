791b6cecf984a7d6a05ba7086baeba384f958394
Tiago Ferreira <ferreiratiago@outlook.com>
2023-02-01 18:21:17 +0000

    fix: go tag

--- integration_test/integration_test.go
+++ integration_test/integration_test.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a license that can be
 // found in the LICENSE file.
 
-//go:test integration
-// +test integration
+//go:build integration
+// +build integration
 
 package integration_test
 
