9b7664697fcd3bbb543c395326e8778bc6ef9ab5
Cheila Alves <calves.200025@gmail.com>
2023-02-01 11:33:52 +0000

    chore: improve error message

--- engine/loader.go
+++ engine/loader.go
@@ -354,8 +354,8 @@ func loadExtension(ctx context.Context, githubClient *gh.GithubClient, extension
 	content, err := githubClient.DownloadContents(ctx, filePath, branch)
 	if err != nil {
 		if responseErr, ok := err.(*github.ErrorResponse); ok && responseErr.Response.StatusCode == 404 {
-			if responseErr.Message == "Repository access blocked" {
-				return nil, "", fmt.Errorf("We were unable to load the Reviewpad configuration due to an authorization error. If you are using extends please make sure you have Reviewpad installed in all extended repositories.")
+			if responseErr.Message == "Not Found" {
+				return nil, "", fmt.Errorf("We encountered an error while loading the Reviewpad configuration. This may be due to reasons such as incorrect file path, invalid URL, or unauthenticated access. Please verify that you have installed Reviewpad in all extended repositories and that the file name and path are correct. If you still encounter this error, reach out to us at #help on https://reviewpad.com/discord for additional support.")
 			} else {
 				return nil, "", err
 			}
