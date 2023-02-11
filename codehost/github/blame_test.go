2a113bd5838beb72584c6d42bb77705461d9a34c
Zelalem Mekonen <zola@programmer.net>
2023-02-11 12:24:05 +0300

    test: add test coverage for slice blame

--- codehost/github/blame_test.go
+++ codehost/github/blame_test.go
@@ -125,3 +125,313 @@ func TestGetGitBlame(t *testing.T) {
 		})
 	}
 }
+
+func TestSliceGitBlame(t *testing.T) {
+	tests := map[string]struct {
+		blame    *github.GitBlame
+		filePath string
+		slices   []github.FileSliceLimits
+		wantRes  *github.GitBlame
+		wantErr  error
+	}{
+		"when file does not exist in blame": {
+			blame: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"reviewpad.yml": {
+						FilePath:  "reviewpad.yml",
+						LineCount: 100,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   100,
+								Author:   "reviewpad[bot]",
+							},
+						},
+					},
+				},
+			},
+			wantErr:  errors.New("file not found in blame information"),
+			filePath: "README.md",
+		},
+		"when line is found": {
+			blame: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"reviewpad.yml": {
+						FilePath:  "reviewpad.yml",
+						LineCount: 100,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   10,
+								Author:   "reviewpad[bot]",
+							},
+							{
+								FromLine: 11,
+								ToLine:   30,
+								Author:   "test",
+							},
+							{
+								FromLine: 31,
+								ToLine:   100,
+								Author:   "reviewpad[bot]",
+							},
+						},
+					},
+				},
+			},
+			wantRes: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"reviewpad.yml": {
+						FilePath:  "reviewpad.yml",
+						LineCount: 100,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 20,
+								ToLine:   25,
+								Author:   "test",
+							},
+						},
+					},
+				},
+			},
+			slices: []github.FileSliceLimits{
+				{
+					From: 20,
+					To:   25,
+				},
+			},
+			filePath: "reviewpad.yml",
+		},
+		"when line is out of range": {
+			blame: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"reviewpad.yml": {
+						FilePath:  "reviewpad.yml",
+						LineCount: 100,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   10,
+								Author:   "reviewpad[bot]",
+							},
+							{
+								FromLine: 11,
+								ToLine:   30,
+								Author:   "test",
+							},
+							{
+								FromLine: 31,
+								ToLine:   100,
+								Author:   "reviewpad[bot]",
+							},
+						},
+					},
+				},
+			},
+			wantRes: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"reviewpad.yml": {
+						FilePath:  "reviewpad.yml",
+						LineCount: 100,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 10,
+								ToLine:   10,
+								Author:   "reviewpad[bot]",
+							},
+							{
+								FromLine: 11,
+								ToLine:   30,
+								Author:   "test",
+							},
+							{
+								FromLine: 31,
+								ToLine:   45,
+								Author:   "reviewpad[bot]",
+							},
+						},
+					},
+				},
+			},
+			slices: []github.FileSliceLimits{
+				{
+					From: 10,
+					To:   45,
+				},
+			},
+			filePath: "reviewpad.yml",
+		},
+		"when multiple files in blame": {
+			blame: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"bar.go": {
+						FilePath:  "bar.go",
+						LineCount: 20,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   10,
+								Author:   "peter",
+							},
+							{
+								FromLine: 11,
+								ToLine:   20,
+								Author:   "maria",
+							},
+						},
+					},
+					"foo.go": {
+						FilePath:  "foo.go",
+						LineCount: 10,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   10,
+								Author:   "john",
+							},
+						},
+					},
+				},
+			},
+			wantRes: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"bar.go": {
+						FilePath:  "bar.go",
+						LineCount: 20,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 10,
+								ToLine:   10,
+								Author:   "peter",
+							},
+							{
+								FromLine: 11,
+								ToLine:   15,
+								Author:   "maria",
+							},
+						},
+					},
+				},
+			},
+			slices: []github.FileSliceLimits{
+				{
+					From: 10,
+					To:   15,
+				},
+			},
+			filePath: "bar.go",
+		},
+		"when multiple slices": {
+			blame: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"bar.go": {
+						FilePath:  "bar.go",
+						LineCount: 50,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   10,
+								Author:   "peter",
+							},
+							{
+								FromLine: 11,
+								ToLine:   20,
+								Author:   "maria",
+							},
+							{
+								FromLine: 21,
+								ToLine:   30,
+								Author:   "jack",
+							},
+							{
+								FromLine: 31,
+								ToLine:   35,
+								Author:   "jill",
+							},
+							{
+								FromLine: 36,
+								ToLine:   45,
+								Author:   "reviewpad[bot]",
+							},
+							{
+								FromLine: 46,
+								ToLine:   50,
+								Author:   "james",
+							},
+						},
+					},
+					"foo.go": {
+						FilePath:  "foo.go",
+						LineCount: 10,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 1,
+								ToLine:   10,
+								Author:   "john",
+							},
+						},
+					},
+				},
+			},
+			wantRes: &github.GitBlame{
+				CommitSHA: "41a7b4350148def9e98c9352a64cc3ac95c1de9c",
+				Files: map[string]github.GitBlameFile{
+					"bar.go": {
+						FilePath:  "bar.go",
+						LineCount: 50,
+						Lines: []github.GitBlameFileLines{
+							{
+								FromLine: 10,
+								ToLine:   10,
+								Author:   "peter",
+							},
+							{
+								FromLine: 11,
+								ToLine:   15,
+								Author:   "maria",
+							},
+							{
+								FromLine: 31,
+								ToLine:   35,
+								Author:   "jill",
+							},
+							{
+								FromLine: 36,
+								ToLine:   45,
+								Author:   "reviewpad[bot]",
+							},
+						},
+					},
+				},
+			},
+			slices: []github.FileSliceLimits{
+				{
+					From: 10,
+					To:   15,
+				},
+				{
+					From: 31,
+					To:   45,
+				},
+			},
+			filePath: "bar.go",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			client := aladino.MockDefaultGithubClient(nil, nil)
+			res, err := client.SliceGitBlame(test.blame, test.filePath, test.slices)
+			assert.Equal(t, test.wantErr, err)
+			assert.Equal(t, test.wantRes, res)
+		})
+	}
+}
