56e07d54b991dfe9cf411d287dcff1008bd84b20
Zelalem Mekonen <zola@programmer.net>
2023-02-11 12:23:16 +0300

    feat: added slice git blame function

--- codehost/github/blame.go
+++ codehost/github/blame.go
@@ -7,6 +7,7 @@ package github
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 )
@@ -26,6 +27,11 @@ type GitBlameFile struct {
 type GitBlame struct {
 	CommitSHA string
 	Files     map[string]GitBlameFile
+}
+
+type FileSliceLimits struct {
+	From uint64
+	To   uint64
 }
 
 type GitBlameQuery struct {
@@ -147,4 +153,51 @@ func mapGitBlameQueryToGitBlame(commitSHA string, gitBlameQuery GitBlameQuery, p
 		CommitSHA: commitSHA,
 		Files:     files,
 	}, nil
+}
+
+func (c *GithubClient) SliceGitBlame(blame *GitBlame, filePath string, slices []FileSliceLimits) (*GitBlame, error) {
+	file, ok := blame.Files[filePath]
+	if !ok {
+		return nil, errors.New("file not found in blame information")
+	}
+
+	slicedBlame := &GitBlame{
+		CommitSHA: blame.CommitSHA,
+		Files:     map[string]GitBlameFile{},
+	}
+
+	gitBlameFile := GitBlameFile{
+		FilePath:  file.FilePath,
+		LineCount: file.LineCount,
+		Lines:     []GitBlameFileLines{},
+	}
+
+	for _, slice := range slices {
+		for _, line := range file.Lines {
+			fromLine := line.FromLine
+			toLine := line.ToLine
+
+			if line.ToLine < slice.From || line.FromLine > slice.To {
+				continue
+			}
+
+			if line.FromLine < slice.From {
+				fromLine = slice.From
+			}
+
+			if line.ToLine > slice.To {
+				toLine = slice.To
+			}
+
+			gitBlameFile.Lines = append(gitBlameFile.Lines, GitBlameFileLines{
+				FromLine: fromLine,
+				ToLine:   toLine,
+				Author:   line.Author,
+			})
+		}
+	}
+
+	slicedBlame.Files[filePath] = gitBlameFile
+
+	return slicedBlame, nil
 }
