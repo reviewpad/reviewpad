61d47c1ec3d4ef94b02fca959cb11c06b04e3d75
Zelalem Mekonen <zola@programmer.net>
2023-02-13 16:47:04 +0300

    revert: unnecessary changes

--- codehost/github/blame.go
+++ codehost/github/blame.go
@@ -33,11 +33,6 @@ type FileSliceLimits struct {
 	To   uint64
 }
 
-type GitBlameAuthorRank struct {
-	Username   string
-	TotalLines int
-}
-
 type GitBlameQuery struct {
 	Repository struct {
 		Object map[string]struct {
