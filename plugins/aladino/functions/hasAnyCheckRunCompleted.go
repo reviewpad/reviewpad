033c80a2287929e25db5cdd94d963ed88ca7f6de
Zelalem Mekonen <zola@programmer.net>
2023-03-03 06:40:13 +0300

    fix: update path to v4

--- plugins/aladino/functions/hasAnyCheckRunCompleted.go
+++ plugins/aladino/functions/hasAnyCheckRunCompleted.go
@@ -8,9 +8,9 @@ import (
 	"fmt"
 
 	"github.com/google/go-github/v49/github"
-	"github.com/reviewpad/reviewpad/v3/codehost/github/target"
-	"github.com/reviewpad/reviewpad/v3/handler"
-	"github.com/reviewpad/reviewpad/v3/lang/aladino"
+	"github.com/reviewpad/reviewpad/v4/codehost/github/target"
+	"github.com/reviewpad/reviewpad/v4/handler"
+	"github.com/reviewpad/reviewpad/v4/lang/aladino"
 )
 
 func HasAnyCheckRunCompleted() *aladino.BuiltInFunction {
