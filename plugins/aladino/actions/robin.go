e7930f0da72f8362bd3491423c1187a4523a31c4
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2023-03-07 09:38:24 +0000

    fix: copyright year

--- plugins/aladino/actions/robin.go
+++ plugins/aladino/actions/robin.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Copyright 2023 Explore.dev Unipessoal Lda. All Rights Reserved.
 // Use of this source code is governed by a license that can be
 // found in the LICENSE file.
 
