a3cccb54ce87e9ce2d732880d8b519687d2a5a49
Zelalem Mekonen <zola@programmer.net>
2023-03-22 09:05:19 +0300

    refactor: revert comment changes

--- plugins/aladino/actions/comment_test.go
+++ plugins/aladino/actions/comment_test.go
@@ -3,3 +3,53 @@
 // found in the LICENSE file.
 
 package plugins_aladino_actions_test
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v49/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/reviewpad/reviewpad/v4/lang/aladino"
+	plugins_aladino "github.com/reviewpad/reviewpad/v4/plugins/aladino"
+	"github.com/reviewpad/reviewpad/v4/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+var comment = plugins_aladino.PluginBuiltIns().Actions["comment"].Code
+
+func TestComment(t *testing.T) {
+	rawComment := "Lorem Ipsum"
+	gotComment := ""
+
+	mockedEnv := aladino.MockDefaultEnv(
+		t,
+		[]mock.MockBackendOption{
+			mock.WithRequestMatch(
+				mock.GetReposIssuesCommentsByOwnerByRepoByIssueNumber,
+				[]*github.IssueComment{},
+			),
+			mock.WithRequestMatchHandler(
+				mock.PostReposIssuesCommentsByOwnerByRepoByIssueNumber,
+				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					rawBody, _ := io.ReadAll(r.Body)
+					body := github.IssueComment{}
+
+					utils.MustUnmarshal(rawBody, &body)
+
+					gotComment = *body.Body
+				}),
+			),
+		},
+		nil,
+		aladino.MockBuiltIns(),
+		nil,
+	)
+
+	args := []aladino.Value{aladino.BuildStringValue(rawComment)}
+	err := comment(mockedEnv, args)
+
+	assert.Nil(t, err)
+	assert.Equal(t, rawComment, gotComment)
+}
