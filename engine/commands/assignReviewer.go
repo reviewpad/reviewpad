dd5f3c350507cf7dc952b539ff6dce1cd82b7450
Zelalem Mekonen <zola@programmer.net>
2023-05-01 13:00:39 +0300

    refactor: assign reviewers command now uses $assignCodeAuthorReviewers built-in

--- engine/commands/assignReviewer.go
+++ engine/commands/assignReviewer.go
@@ -5,10 +5,8 @@
 package commands
 
 import (
-	"errors"
 	"fmt"
-	"regexp"
-	"strings"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -25,8 +23,8 @@ func AssignReviewerCmd() *cobra.Command {
 				return fmt.Errorf("accepts 1 arg(s), received %d", len(args))
 			}
 
-			if !regexp.MustCompile(`^([a-zA-Z0-9\-]+[a-zA-Z0-9])(,[a-zA-Z0-9\-]+[a-zA-Z0-9])*$`).MatchString(args[0]) {
-				return errors.New("reviewers must be a list of comma separated valid github usernames")
+			if _, err := strconv.ParseUint(args[0], 10, 64); err != nil {
+				return fmt.Errorf("invalid argument: %s, number of reviewers must be a number", args[0])
 			}
 
 			return nil
@@ -34,41 +32,20 @@ func AssignReviewerCmd() *cobra.Command {
 		RunE: AssignReviewer,
 	}
 
-	flags := assignReviewerCmd.Flags()
-
-	flags.StringP("review-policy", "p", "reviewpad", "The policy followed for reviewer assignment. The valid values can only be: random, round-robin, reviewpad. By default, the policy is reviewpad.")
-
-	flags.Uint8P("total-reviewers", "t", 0, "The total number of reviewers to assign to. By default, it assigns to all reviewers.")
-
 	return assignReviewerCmd
 }
 
 func AssignReviewer(cmd *cobra.Command, args []string) error {
-	flags := cmd.Flags()
-
-	reviewersList := strings.Split(strings.ReplaceAll(args[0], " ", ""), ",")
-
-	availableReviewers := `"` + strings.Join(reviewersList, `","`) + `"`
-
-	totalRequiredReviewers, err := flags.GetUint8("total-reviewers")
+	totalRequiredReviewers, err := strconv.ParseUint(args[0], 10, 64)
 	if err != nil {
 		return err
 	}
 
 	if totalRequiredReviewers == 0 {
-		totalRequiredReviewers = uint8(len(reviewersList))
+		return fmt.Errorf("invalid argument: %d, number of reviewers must be greater than 0", totalRequiredReviewers)
 	}
 
-	policy, err := flags.GetString("review-policy")
-	if err != nil {
-		return err
-	}
-
-	if policy != "reviewpad" && policy != "round-robin" && policy != "random" {
-		return fmt.Errorf("invalid review policy specified: %s", policy)
-	}
-
-	action := fmt.Sprintf(`$assignReviewer([%s], %d, %q)`, availableReviewers, totalRequiredReviewers, policy)
+	action := fmt.Sprintf(`$assignCodeAuthorReviewers(%d, [], 0)`, totalRequiredReviewers)
 
 	cmd.Print(action)
 
