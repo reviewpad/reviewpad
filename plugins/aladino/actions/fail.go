f5c8995ed6f72b4c20481aa740e58e96aaa5e9d5
Zelalem Mekonen <zola@programmer.net>
2023-05-19 10:26:10 +0300

    feat: stack fail messages along with failCheckStatus messages and display on check run

--- plugins/aladino/actions/fail.go
+++ plugins/aladino/actions/fail.go
@@ -5,6 +5,10 @@
 package plugins_aladino_actions
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/google/go-github/v52/github"
 	"github.com/reviewpad/go-lib/entities"
 	"github.com/reviewpad/reviewpad/v4/lang/aladino"
 )
@@ -18,9 +22,34 @@ func Fail() *aladino.BuiltInAction {
 }
 
 func failCode(e aladino.Env, args []aladino.Value) error {
+	targetEntity := e.GetTarget().GetTargetEntity()
 	failMessage := args[0].(*aladino.StringValue).Val
 
 	e.GetBuiltInsReportedMessages()[aladino.SEVERITY_FATAL] = append(e.GetBuiltInsReportedMessages()[aladino.SEVERITY_FATAL], failMessage)
+	// Because we want to stack the messages from $failCheckStatus and $fail as well, we are going to add the message to the fail list as well.
+	e.GetBuiltInsReportedMessages()[aladino.SEVERITY_FAIL] = append(e.GetBuiltInsReportedMessages()[aladino.SEVERITY_FAIL], failMessage)
+
+	if e.GetCheckRunID() != nil {
+		e.SetCheckRunConclusion("failure")
+
+		summary := strings.Builder{}
+		summary.WriteString("#### Here are the reasons for Reviewpad's failure:\n\n")
+
+		for _, msg := range e.GetBuiltInsReportedMessages()[aladino.SEVERITY_FAIL] {
+			summary.WriteString(fmt.Sprintf("- %s\n", msg))
+		}
+
+		_, _, err := e.GetGithubClient().GetClientREST().Checks.UpdateCheckRun(e.GetCtx(), targetEntity.Owner, targetEntity.Repo, *e.GetCheckRunID(), github.UpdateCheckRunOptions{
+			Name:       "reviewpad",
+			Status:     github.String("completed"),
+			Conclusion: github.String("failure"),
+			Output: &github.CheckRunOutput{
+				Title:   github.String("Reviewpad failed"),
+				Summary: github.String(summary.String()),
+			},
+		})
+		return err
+	}
 
 	return nil
 }
