292787c17a5fd28ff2b3df16cb80d48931342622
Tiago Ferreira <ferreiratiago@outlook.com>
2023-07-26 14:43:45 +0100

    fix: example url for extends error

--- utils/url.go
+++ utils/url.go
@@ -11,7 +11,7 @@ func ValidateUrl(fileUrl string) (*pbc.Branch, string, error) {
 	re := regexp.MustCompile(`^https:\/\/github\.com\/([^/]+)/([^/]+)/blob/([^/]+)/(.+)$`)
 	result := re.FindStringSubmatch(fileUrl)
 	if len(result) != 5 {
-		return nil, "", errors.New("fatal: url must be a link to a GitHub blob, e.g. https://github.com/reviewpad/action/blob/main/main.go")
+		return nil, "", errors.New("fatal: url must be a link to a GitHub blob, e.g. https://github.com/reviewpad/reviewpad/blob/main/reviewpad.yml")
 	}
 
 	branch := &pbc.Branch{
