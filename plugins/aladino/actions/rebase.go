dfb652708ea6818d39e88613541696a18091148d
marcelosousa <marcelosousa@prx9pgfmv2.home>
2024-03-04 11:43:06 +0000

    test: remove libgit2

--- plugins/aladino/actions/rebase.go
+++ plugins/aladino/actions/rebase.go
@@ -5,13 +5,7 @@
 package plugins_aladino_actions
 
 import (
-	"errors"
-	"os"
-
-	git "github.com/libgit2/git2go/v31"
 	"github.com/reviewpad/go-lib/entities"
-	gh "github.com/reviewpad/reviewpad/v4/codehost/github"
-	"github.com/reviewpad/reviewpad/v4/codehost/github/target"
 	"github.com/reviewpad/reviewpad/v4/lang"
 	"github.com/reviewpad/reviewpad/v4/lang/aladino"
 )
@@ -25,41 +19,5 @@ func Rebase() *aladino.BuiltInAction {
 }
 
 func rebaseCode(e aladino.Env, args []lang.Value) error {
-	githubToken := e.GetGithubClient().GetToken()
-	log := e.GetLogger().WithField("builtin", "rebase")
-	t := e.GetTarget().(*target.PullRequestTarget)
-	pr := t.PullRequest
-
-	if !pr.IsRebaseable {
-		return errors.New("the pull request is not rebaseable")
-	}
-
-	headHTMLUrl := pr.Head.GetRepo().GetUri()
-	headRef := pr.Head.Name
-	baseRef := pr.Base.Name
-
-	repo, dir, err := gh.CloneRepository(log, headHTMLUrl, githubToken, "", &git.CloneOptions{
-		CheckoutBranch: baseRef,
-	})
-	if err != nil {
-		return err
-	}
-	defer os.RemoveAll(dir)
-
-	err = gh.CheckoutBranch(log, repo, headRef)
-	if err != nil {
-		return err
-	}
-
-	err = gh.RebaseOnto(log, repo, baseRef, nil)
-	if err != nil {
-		return err
-	}
-
-	err = gh.Push(log, repo, "origin", headRef, githubToken, true)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
